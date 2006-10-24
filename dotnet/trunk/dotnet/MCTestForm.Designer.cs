1db5156a5c2e74bc850e74dbfab18f19433f06a4
davidthings <davidthings@users.noreply.github.com>
2006-10-24 21:48:56 +0000

    new .net stuff

---
+++ dotnet/trunk/dotnet/MCTestForm.Designer.cs
@@ -0,0 +1,294 @@
+namespace MakingThings
+{
+    partial class MCTestForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+          this.InputPanes = new System.Windows.Forms.TabControl();
+          this.Usb = new System.Windows.Forms.TabPage();
+          this.label6 = new System.Windows.Forms.Label();
+          this.Command = new System.Windows.Forms.Label();
+          this.UsbSend = new System.Windows.Forms.Button();
+          this.UsbCommand = new System.Windows.Forms.TextBox();
+          this.UsbPortName = new System.Windows.Forms.TextBox();
+          this.Udp = new System.Windows.Forms.TabPage();
+          this.UdpCommand = new System.Windows.Forms.TextBox();
+          this.UdpSend = new System.Windows.Forms.Button();
+          this.label5 = new System.Windows.Forms.Label();
+          this.UdpListeningPort = new System.Windows.Forms.TextBox();
+          this.UdpToPort = new System.Windows.Forms.TextBox();
+          this.UdpToAddress = new System.Windows.Forms.TextBox();
+          this.label4 = new System.Windows.Forms.Label();
+          this.label3 = new System.Windows.Forms.Label();
+          this.label2 = new System.Windows.Forms.Label();
+          this.Output = new System.Windows.Forms.RichTextBox();
+          this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+          this.label1 = new System.Windows.Forms.Label();
+          this.InputPanes.SuspendLayout();
+          this.Usb.SuspendLayout();
+          this.Udp.SuspendLayout();
+          this.tableLayoutPanel1.SuspendLayout();
+          this.SuspendLayout();
+          // 
+          // InputPanes
+          // 
+          this.InputPanes.Controls.Add(this.Usb);
+          this.InputPanes.Controls.Add(this.Udp);
+          this.InputPanes.Location = new System.Drawing.Point(3, 31);
+          this.InputPanes.Name = "InputPanes";
+          this.InputPanes.SelectedIndex = 0;
+          this.InputPanes.Size = new System.Drawing.Size(632, 134);
+          this.InputPanes.TabIndex = 0;
+          // 
+          // Usb
+          // 
+          this.Usb.Controls.Add(this.label6);
+          this.Usb.Controls.Add(this.Command);
+          this.Usb.Controls.Add(this.UsbSend);
+          this.Usb.Controls.Add(this.UsbCommand);
+          this.Usb.Controls.Add(this.UsbPortName);
+          this.Usb.Location = new System.Drawing.Point(4, 25);
+          this.Usb.Name = "Usb";
+          this.Usb.Padding = new System.Windows.Forms.Padding(3);
+          this.Usb.Size = new System.Drawing.Size(624, 105);
+          this.Usb.TabIndex = 0;
+          this.Usb.Text = "USB";
+          this.Usb.UseVisualStyleBackColor = true;
+          // 
+          // label6
+          // 
+          this.label6.AutoSize = true;
+          this.label6.Location = new System.Drawing.Point(10, 20);
+          this.label6.Name = "label6";
+          this.label6.Size = new System.Drawing.Size(34, 17);
+          this.label6.TabIndex = 4;
+          this.label6.Text = "Port";
+          // 
+          // Command
+          // 
+          this.Command.AutoSize = true;
+          this.Command.Location = new System.Drawing.Point(10, 55);
+          this.Command.Name = "Command";
+          this.Command.Size = new System.Drawing.Size(71, 17);
+          this.Command.TabIndex = 3;
+          this.Command.Text = "Command";
+          // 
+          // UsbSend
+          // 
+          this.UsbSend.Location = new System.Drawing.Point(525, 54);
+          this.UsbSend.Name = "UsbSend";
+          this.UsbSend.Size = new System.Drawing.Size(75, 23);
+          this.UsbSend.TabIndex = 2;
+          this.UsbSend.Text = "Send";
+          this.UsbSend.UseVisualStyleBackColor = true;
+          this.UsbSend.Click += new System.EventHandler(this.UsbSend_Click);
+          // 
+          // UsbCommand
+          // 
+          this.UsbCommand.Location = new System.Drawing.Point(90, 55);
+          this.UsbCommand.Name = "UsbCommand";
+          this.UsbCommand.Size = new System.Drawing.Size(425, 22);
+          this.UsbCommand.TabIndex = 1;
+          this.UsbCommand.KeyUp += new System.Windows.Forms.KeyEventHandler(this.UsbCommand_KeyUp);
+          // 
+          // UsbPortName
+          // 
+          this.UsbPortName.Location = new System.Drawing.Point(90, 20);
+          this.UsbPortName.Name = "UsbPortName";
+          this.UsbPortName.Size = new System.Drawing.Size(100, 22);
+          this.UsbPortName.TabIndex = 0;
+          // 
+          // Udp
+          // 
+          this.Udp.Controls.Add(this.UdpCommand);
+          this.Udp.Controls.Add(this.UdpSend);
+          this.Udp.Controls.Add(this.label5);
+          this.Udp.Controls.Add(this.UdpListeningPort);
+          this.Udp.Controls.Add(this.UdpToPort);
+          this.Udp.Controls.Add(this.UdpToAddress);
+          this.Udp.Controls.Add(this.label4);
+          this.Udp.Controls.Add(this.label3);
+          this.Udp.Controls.Add(this.label2);
+          this.Udp.Location = new System.Drawing.Point(4, 25);
+          this.Udp.Name = "Udp";
+          this.Udp.Padding = new System.Windows.Forms.Padding(3);
+          this.Udp.Size = new System.Drawing.Size(624, 105);
+          this.Udp.TabIndex = 1;
+          this.Udp.Text = "UDP";
+          this.Udp.UseVisualStyleBackColor = true;
+          // 
+          // UdpCommand
+          // 
+          this.UdpCommand.Location = new System.Drawing.Point(90, 55);
+          this.UdpCommand.Name = "UdpCommand";
+          this.UdpCommand.Size = new System.Drawing.Size(425, 22);
+          this.UdpCommand.TabIndex = 8;
+          this.UdpCommand.KeyUp += new System.Windows.Forms.KeyEventHandler(this.UdpCommand_KeyUp);
+          // 
+          // UdpSend
+          // 
+          this.UdpSend.Location = new System.Drawing.Point(525, 54);
+          this.UdpSend.Name = "UdpSend";
+          this.UdpSend.Size = new System.Drawing.Size(75, 23);
+          this.UdpSend.TabIndex = 7;
+          this.UdpSend.Text = "Send";
+          this.UdpSend.UseVisualStyleBackColor = true;
+          this.UdpSend.Click += new System.EventHandler(this.UdpSend_Click);
+          // 
+          // label5
+          // 
+          this.label5.AutoSize = true;
+          this.label5.Location = new System.Drawing.Point(10, 55);
+          this.label5.Name = "label5";
+          this.label5.Size = new System.Drawing.Size(71, 17);
+          this.label5.TabIndex = 6;
+          this.label5.Text = "Command";
+          // 
+          // UdpListeningPort
+          // 
+          this.UdpListeningPort.Location = new System.Drawing.Point(501, 17);
+          this.UdpListeningPort.Name = "UdpListeningPort";
+          this.UdpListeningPort.Size = new System.Drawing.Size(100, 22);
+          this.UdpListeningPort.TabIndex = 5;
+          // 
+          // UdpToPort
+          // 
+          this.UdpToPort.Location = new System.Drawing.Point(276, 20);
+          this.UdpToPort.Name = "UdpToPort";
+          this.UdpToPort.Size = new System.Drawing.Size(100, 22);
+          this.UdpToPort.TabIndex = 4;
+          // 
+          // UdpToAddress
+          // 
+          this.UdpToAddress.Location = new System.Drawing.Point(90, 20);
+          this.UdpToAddress.Name = "UdpToAddress";
+          this.UdpToAddress.Size = new System.Drawing.Size(100, 22);
+          this.UdpToAddress.TabIndex = 3;
+          // 
+          // label4
+          // 
+          this.label4.AutoSize = true;
+          this.label4.Location = new System.Drawing.Point(400, 20);
+          this.label4.Name = "label4";
+          this.label4.Size = new System.Drawing.Size(95, 17);
+          this.label4.TabIndex = 2;
+          this.label4.Text = "Listening Port";
+          // 
+          // label3
+          // 
+          this.label3.AutoSize = true;
+          this.label3.Location = new System.Drawing.Point(215, 20);
+          this.label3.Name = "label3";
+          this.label3.Size = new System.Drawing.Size(55, 17);
+          this.label3.TabIndex = 1;
+          this.label3.Text = "To Port";
+          // 
+          // label2
+          // 
+          this.label2.AutoSize = true;
+          this.label2.Location = new System.Drawing.Point(10, 20);
+          this.label2.Name = "label2";
+          this.label2.Size = new System.Drawing.Size(81, 17);
+          this.label2.TabIndex = 0;
+          this.label2.Text = "To Address";
+          // 
+          // Output
+          // 
+          this.Output.Location = new System.Drawing.Point(3, 173);
+          this.Output.Name = "Output";
+          this.Output.Size = new System.Drawing.Size(632, 326);
+          this.Output.TabIndex = 1;
+          this.Output.Text = "";
+          // 
+          // tableLayoutPanel1
+          // 
+          this.tableLayoutPanel1.ColumnCount = 1;
+          this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+          this.tableLayoutPanel1.Controls.Add(this.InputPanes, 0, 1);
+          this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+          this.tableLayoutPanel1.Controls.Add(this.Output, 0, 2);
+          this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+          this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+          this.tableLayoutPanel1.RowCount = 3;
+          this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 28F));
+          this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30.12048F));
+          this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 69.87952F));
+          this.tableLayoutPanel1.Size = new System.Drawing.Size(638, 502);
+          this.tableLayoutPanel1.TabIndex = 2;
+          // 
+          // label1
+          // 
+          this.label1.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+          this.label1.Location = new System.Drawing.Point(3, 0);
+          this.label1.Name = "label1";
+          this.label1.Size = new System.Drawing.Size(632, 28);
+          this.label1.TabIndex = 2;
+          this.label1.Text = "Make Controller Kit Helper";
+          this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+          // 
+          // MCTestForm
+          // 
+          this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+          this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+          this.ClientSize = new System.Drawing.Size(662, 529);
+          this.Controls.Add(this.tableLayoutPanel1);
+          this.Name = "MCTestForm";
+          this.Text = "MCTest";
+          this.InputPanes.ResumeLayout(false);
+          this.Usb.ResumeLayout(false);
+          this.Usb.PerformLayout();
+          this.Udp.ResumeLayout(false);
+          this.Udp.PerformLayout();
+          this.tableLayoutPanel1.ResumeLayout(false);
+          this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl InputPanes;
+        private System.Windows.Forms.TabPage Usb;
+        private System.Windows.Forms.TabPage Udp;
+        private System.Windows.Forms.RichTextBox Output;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox UdpCommand;
+        private System.Windows.Forms.Button UdpSend;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox UdpListeningPort;
+        private System.Windows.Forms.TextBox UdpToPort;
+        private System.Windows.Forms.TextBox UdpToAddress;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label Command;
+        private System.Windows.Forms.Button UsbSend;
+        private System.Windows.Forms.TextBox UsbCommand;
+        private System.Windows.Forms.TextBox UsbPortName;
+    }
+}
---
+++ dotnet/trunk/dotnet/Osc.cs
@@ -0,0 +1,279 @@
+using System.Threading;
+using System.Text;
+using System.Collections;
+using System;
+
+
+namespace MakingThings
+{
+  public class OscMessage
+  {
+    public OscMessage()
+    {
+      Values = new ArrayList();
+    }
+      
+   public string Address;
+   public ArrayList Values;
+  }
+
+  public class Osc
+  {
+    public Osc()
+    {
+    }
+
+    public static string OscMessageToString(OscMessage message)
+    {
+      StringBuilder s = new StringBuilder();
+      s.Append(message.Address);
+      foreach( object o in message.Values )
+      {
+        s.Append(" ");
+        s.Append(o.ToString());
+      }
+      return s.ToString();
+    }
+
+    public static OscMessage StringToOscMessage(string message)
+    {
+      OscMessage oM = new OscMessage();
+      Console.WriteLine("Splitting " + message);
+      string[] ss = message.Split(new char[] { ' ' });
+      IEnumerator sE = ss.GetEnumerator();
+      if (sE.MoveNext())
+        oM.Address = (string)sE.Current;
+      while ( sE.MoveNext() )
+      {
+        string s = (string)sE.Current;
+        Console.WriteLine("  <" + s + ">");
+        if (s.StartsWith("\""))
+        {
+          StringBuilder quoted = new StringBuilder();
+          bool looped = false;
+          if (s.Length > 1)
+            quoted.Append(s.Substring(1));
+          else
+            looped = true;
+          while (sE.MoveNext())
+          {
+            string a = (string)sE.Current;
+            Console.WriteLine("    q:<" + a + ">");
+            if (looped)
+              quoted.Append(" ");
+            if (a.EndsWith("\""))
+            {
+              quoted.Append(a.Substring(0, a.Length - 1));
+              break;
+            }
+            else
+            {
+              if (a.Length == 0)
+                quoted.Append(" ");
+              else
+                quoted.Append(a);
+            }
+            looped = true;
+          }
+          oM.Values.Add(quoted.ToString());
+        }
+        else
+        {
+          if (s.Length > 0)
+          {
+            try
+            {
+              int i = int.Parse(s);
+              Console.WriteLine("  i:" + i);
+              oM.Values.Add(i);
+            }
+            catch
+            {
+              try
+              {
+                float f = float.Parse(s);
+                Console.WriteLine("  f:" + f);
+                oM.Values.Add(f);
+              }
+              catch
+              {
+                Console.WriteLine("  s:" + s);
+                oM.Values.Add(s);
+              }
+            }
+
+          }
+        }
+      }
+
+      return oM;
+    }
+
+    public static ArrayList PacketToOscMessages(byte[] packet, int length)
+    {
+      ArrayList messages = new ArrayList();
+      ExtractMessages(messages, packet, 0, length);
+      return messages;
+    }
+
+    public static int OscMessagesToPacket(ArrayList messages, byte[] packet, int length)
+    {
+      return 0;
+    }
+
+    public static int OscMessageToPacket(OscMessage oscM, byte[] packet, int length)
+    {
+      return OscMessageToPacket(oscM, packet, 0, length);
+    }
+
+    private static int OscMessageToPacket(OscMessage oscM, byte[] packet, int start, int length)
+    {
+      int index = start;
+      index = InsertString(oscM.Address, packet, index, length);
+      StringBuilder tag = new StringBuilder();
+      tag.Append(",");
+      int tagIndex = index;
+      index += PadSize(1 + oscM.Values.Count);
+      foreach (object o in oscM.Values)
+      {
+        if (o is int)
+        {
+          int i = (int)o;
+          tag.Append("i");
+          packet[index++] = (byte)((i >> 24) & 0xFF);
+          packet[index++] = (byte)((i >> 16) & 0xFF);
+          packet[index++] = (byte)((i >> 8) & 0xFF);
+          packet[index++] = (byte)((i) & 0xFF);
+        }
+        else
+        {
+          if (o is float)
+          {
+            float f = (float)o;
+            tag.Append("f");
+            packet[index++] = 0;
+            packet[index++] = 0;
+            packet[index++] = 0;
+            packet[index++] = 0;
+          }
+          else
+          {
+            if (o is string)
+            {
+              tag.Append("s");
+              index = InsertString(o.ToString(), packet, index, length);
+            }
+            else
+            {
+              tag.Append("?");
+            }
+          }
+        }
+      }
+      InsertString(tag.ToString(), packet, tagIndex, length);
+      return index;
+    }
+
+    private static int ExtractMessages(ArrayList messages, byte[] packet, int start, int length)
+    {
+      int index = start;
+      switch ( (char)packet[ start ] )
+      {
+        case '/':
+          index = ExtractMessage( messages, packet, index, length );
+          break;
+        case '#':
+          string bundleString = ExtractString(packet, start, length);
+          if ( bundleString == "#bundle" )
+          {
+            // skip the "bundle" and the timestamp
+            index+=16;
+            while ( index < length )
+            {
+              int messageSize = ( packet[index++] << 24 ) + ( packet[index++] << 16 ) + ( packet[index++] << 8 ) + packet[index++];
+              int newIndex = ExtractMessages( messages, packet, index, length ); 
+              index += messageSize;
+            }            
+          }
+          break;
+      }
+      return index;
+    }
+
+    private static int ExtractMessage(ArrayList messages, byte[] packet, int start, int length)
+    {
+      OscMessage oscM = new OscMessage();
+      oscM.Address = ExtractString(packet, start, length);
+      int index = start + PadSize(oscM.Address.Length+1);
+      string typeTag = ExtractString(packet, index, length);
+      index += PadSize(typeTag.Length + 1);
+      //oscM.Values.Add(typeTag);
+      foreach (char c in typeTag)
+      {
+        switch (c)
+        {
+          case ',':
+            break;
+          case 's':
+            {
+              string s = ExtractString(packet, index, length);
+              index += PadSize(s.Length + 1);
+              oscM.Values.Add(s);
+              break;
+            }
+          case 'i':
+            {
+              int i = ( packet[index++] << 24 ) + ( packet[index++] << 16 ) + ( packet[index++] << 8 ) + packet[index++];
+              oscM.Values.Add(i);
+              break;
+            }
+          case 'f':
+            {
+              float f = 1.1F;
+              oscM.Values.Add(f);
+              break;
+            }
+        }
+      }
+      messages.Add( oscM );
+      return index;
+    }
+
+    private static string ExtractString(byte[] packet, int start, int length)
+    {
+      StringBuilder sb = new StringBuilder();
+      int index = start;
+      while (packet[index] != 0 && index < length)
+        sb.Append((char)packet[index++]);
+      return sb.ToString();
+    }
+
+    private static int InsertString(string s, byte[] packet, int start, int length)
+    {
+      int index = start;
+      foreach (char c in s)
+      {
+        packet[index++] = (byte)c;
+        if (index == length)
+          return index;
+      }
+      packet[index++] = 0;
+      int pad = s.Length % 4;
+      if (pad != 0)
+      {
+        while (pad-- > 0)
+          packet[index++] = 0;
+      }
+      return index;
+    }
+
+    private int PadSize(int rawSize)
+    {
+      int pad = rawSize % 4;
+      if (pad == 0)
+        return rawSize;
+      else
+        return rawSize + (4 - pad);
+    }
+  }
+}
