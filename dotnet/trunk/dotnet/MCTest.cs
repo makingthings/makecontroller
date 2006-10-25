f3a47cb013830c92544c5fca886c685706db3df6
davidthings <davidthings@388e45b6-652a-11de-9f54-6d4fae1110e3>
2006-10-25 05:25:07 +0000

    Simplified interface by moving the thread code into Osc

--- dotnet/trunk/dotnet/MCTest.cs
+++ dotnet/trunk/dotnet/MCTest.cs
@@ -13,150 +13,63 @@ public class MCTest
 
   MCTest()
   {
+    mct = new MCTestForm(this);
+
     usbPacket = new UsbPacket();
-    usbPacket.Open();
+    oscUsb = null;
+    //oscUsb = new Osc(usbPacket);
+    //oscUsb.SetAllMessageHandler(UsbMessages);
 
     udpPacket = new UdpPacket();
-    udpPacket.Open();
-   
-    mct = new MCTestForm( this );
-    osc = new Osc();
-
-    Running = true;
-    if (usbPacket.IsOpen())
-    {
-      mct.SetUsbPortName(usbPacket.Name);
-
-      usbReadThread = new Thread(UsbRead);
-      usbReadThread.Start();
-    }
-
-    if (udpPacket.IsOpen())
-    {
-      udpReadThread = new Thread(UdpRead);
-      udpReadThread.Start();
-    }
+    oscUdp = new Osc(udpPacket);
+    oscUdp.SetAllMessageHandler(UdpMessages);
+    oscUdp.SetAddressHandler("/analogin/0/value", AIn0Message);
 
     Application.Run(mct);
   }
-  
-  /// <summary>
-  /// UsbRead() reads a packet from the USB/serial port, and if successful prints the results to the console.
-  /// </summary>
-  public void UsbRead()
-  {
-    while ( Running )
-    {
-      if ( usbPacket.IsOpen() )
-      {
-        try
-        {
-          byte[] buffer = new byte[ 1000 ];
-          int length = usbPacket.ReceivePacket( buffer );
-          if (length > 0)
-          {
-            Console.WriteLine("  Packet Received");
-            Console.Write("  ");
-            for (int i = 0; i < length; i++)
-            {
-              Console.Write(buffer[i]);
-              Console.Write(' ');
-            }
-            Console.WriteLine();
-          }
-        }
-        catch (TimeoutException) { }
-      }
-      else
-      {
-        usbPacket.Open();
-        if (!usbPacket.IsOpen())
-          Thread.Sleep(100);
-      }
-    }
-  }
-  
-  /// <summary>
-  /// UdpRead() reads a UDP packet, and if successful prints the results to the console.
-  /// </summary>
-  public void UdpRead()
-  {
-    while (Running)
-    {
-      if (udpPacket.IsOpen())
-      {
-        try
-        {
-          byte[] buffer = new byte[1000];
-          int length = udpPacket.ReceivePacket(buffer);
-          if (length > 0)
-          {
-            ArrayList messages = Osc.PacketToOscMessages(buffer, length);
-            foreach (OscMessage om in messages)
-            {
-              string message = Osc.OscMessageToString(om);
-              //Console.WriteLine("Received Message: " + message );
-              mct.WriteLine("UDP>"+message);
-            }
-          }
-        }
-        catch (TimeoutException) { }
-      }
-      else
-      {
-        udpPacket.Open();
-        if (!udpPacket.IsOpen())
-          Thread.Sleep(100);
-      }
-    }
-  }
 
-  /// <summary>
-  /// usbSend() writes a string to the USB/serial port.
-  /// </summary>
-  /// <param name="text">The string to be written.</param>
   public void usbSend(string text)
   {
-    if ( !usbPacket.IsOpen() )
-      usbPacket.Open();
-    if (usbPacket.IsOpen())
-    {
-      mct.WriteLine("USB<" + text);
-      byte[] buffer = new byte[1000];
-
-      OscMessage oscM = Osc.StringToOscMessage(text);
-      int length = Osc.OscMessageToPacket(oscM, buffer, 1000);
-      usbPacket.SendPacket(buffer, length);
-    }
+    mct.WriteLine("USB < " + text);
+    OscMessage oscM = Osc.StringToOscMessage(text);
+    oscUsb.Send(oscM);
   }
 
-  /// <summary>
-  /// udpSend() writes a string over Ethernet in a UDP packet.
-  /// </summary>
-  /// <param name="text">The string to be written.</param>
   public void udpSend(string text)
   {
-    if (!udpPacket.IsOpen())
-      udpPacket.Open();
-    if (udpPacket.IsOpen())
-    {
-      mct.WriteLine("UDP<" + text );
-      byte[] buffer = new byte[1000];
+    mct.WriteLine("UDP < " + text);
+    OscMessage oscM = Osc.StringToOscMessage(text);
+    oscUdp.Send(oscM);
 
-      OscMessage oscM = Osc.StringToOscMessage(text);
-      int length = Osc.OscMessageToPacket(oscM, buffer, 1000);
-      udpPacket.SendPacket(buffer, length);
-    }
+    mct.WriteLine("UDP+< " + text);
+    OscMessage oscM1 = Osc.StringToOscMessage(text);
+    OscMessage oscM2 = Osc.StringToOscMessage("/network/address");
+    ArrayList l = new ArrayList();
+    l.Add(oscM1);
+    l.Add(oscM2);
+    oscUdp.Send(l);
   }
 
-  private bool Running;
-  UsbPacket usbPacket;
-  UdpPacket udpPacket;
+  public void UdpMessages(OscMessage oscMessage)
+  {
+    mct.WriteLine( "UDP > " + Osc.OscMessageToString(oscMessage) );
+  }
 
-  Thread udpReadThread;
-  Thread usbReadThread;
+  public void UsbMessages(OscMessage oscMessage)
+  {
+    mct.WriteLine("USB > " + Osc.OscMessageToString(oscMessage));
+  }
 
-  Osc osc;
+  public void AIn0Message(OscMessage oscMessage)
+  {
+    mct.WriteLine("AIn0 > " + Osc.OscMessageToString(oscMessage));
+  }
+
+  private UsbPacket usbPacket;
+  private UdpPacket udpPacket;
+
+  private Osc oscUdp;
+  private Osc oscUsb;
 
   MCTestForm mct;
 }
