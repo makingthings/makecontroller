790b6c2e317e2824e6c8952ac0f1e76038dd67c7
davidthings <davidthings@users.noreply.github.com>
2006-11-08 20:12:56 +0000

    New Sample Code for MS'ers

---
+++ dotnet/trunk/QuickTest/QuickTest.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using MakingThings;
+
+namespace QuickTest
+{
+  public partial class QuickTest : Form
+  {
+    public QuickTest()
+    {
+      InitializeComponent();
+
+      udpPacket = new UdpPacket();
+      udpPacket.RemoteHostName = "192.168.0.200";
+      udpPacket.RemotePort = 10000;
+      udpPacket.LocalPort = 10000;
+      udpPacket.Open();
+      oscUdp = new Osc(udpPacket);
+
+      oscUdp.SetAddressHandler("/analogin/7/value", TrimPotReading);
+    }
+
+    // This delegate enables asynchronous calls for setting
+    // the value property on the Indicator.
+    delegate void IndicatorCallback(int value);
+
+    void TrimPotReading(OscMessage oscM)
+    {      
+      int value = (int)oscM.Values[0];
+      SetIndicator( value );
+    }
+
+    public void SetIndicator(int value)
+    {
+      // InvokeRequired required compares the thread ID of the
+      // calling thread to the thread ID of the creating thread.
+      // If these threads are different, it returns true.
+      if (TrimPotIndicator.InvokeRequired)
+      {
+        IndicatorCallback d = new IndicatorCallback(SetIndicator);
+        this.Invoke(d, new object[] { value });
+      }
+      else
+      {
+        TrimPotIndicator.Value = value;
+      }
+    }
+
+    private UdpPacket udpPacket;
+
+    private Osc oscUdp;
+
+    private void timer1_Tick(object sender, EventArgs e)
+    {
+      OscMessage oscMS = new OscMessage();
+      oscMS.Address = "/analogin/7/value";
+      oscUdp.Send(oscMS);
+    }
+
+    private void Led0_CheckedChanged(object sender, EventArgs e)
+    {
+      LedSet(0, Led0.Checked);
+    }
+
+    private void Led1_CheckedChanged(object sender, EventArgs e)
+    {
+      LedSet(1, Led1.Checked);
+    }
+
+    private void Led2_CheckedChanged(object sender, EventArgs e)
+    {
+      LedSet(2, Led2.Checked);
+    }
+
+    private void Led3_CheckedChanged(object sender, EventArgs e)
+    {
+      LedSet(3, Led3.Checked);
+    }
+
+    private void LedSet(int index, bool value)
+    {
+      OscMessage oscMS = new OscMessage();
+      oscMS.Address = "/appled/" + index.ToString() + "/state";
+      oscMS.Values.Add(value?1:0);
+      oscUdp.Send(oscMS);
+    }
+  }
+}
