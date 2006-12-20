84140b3b0e7078ea434c25cc24e346974552614a
davidthings <davidthings@users.noreply.github.com>
2006-12-20 20:30:29 +0000

    Gave the OSC UDP task some more stack.  A little much, maybe...

--- firmware/trunk/controller/makingthings/osc.c
+++ firmware/trunk/controller/makingthings/osc.c
@@ -206,7 +206,7 @@ int Osc_Start( void )
     if ( Osc.channel[ OSC_CHANNEL_UDP ].replyPort == 0 )
       Osc_SetReplyPort( OSC_CHANNEL_UDP, 10000 );
 
-    TaskCreate( Osc_UdpTask, "OSC-UDP", 300, (void*)OSC_CHANNEL_UDP, 3 );
+    TaskCreate( Osc_UdpTask, "OSC-UDP", 500, (void*)OSC_CHANNEL_UDP, 3 );
     TaskCreate( Osc_UsbTask, "OSC-USB", 300, (void*)OSC_CHANNEL_USB, 3 );
 
     Osc.users++;
