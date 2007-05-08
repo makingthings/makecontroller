6f1d90e0d39fba42541888c0d8ac847a2ca3a368
liamstask <liamstask@388e45b6-652a-11de-9f54-6d4fae1110e3>
2007-05-08 19:11:18 +0000

    Removed file/folder

--- firmware/branches/firmware-1.2a/test/intermediate/crt0.d
+++
@@ -1,2 +0,0 @@
-intermediate/crt0.o:  \
- C:/mtcode/make/firmware/test/../controller/startup/crt0.s
--- firmware/branches/firmware-1.2a/test/intermediate/osc_patternmatch.d
+++
@@ -1,4 +0,0 @@
-intermediate/osc_patternmatch.o:  \
- C:/mtcode/make/firmware/test/../controller/makingthings/osc_patternmatch.c \
-  C:/mtcode/make/firmware/test/../controller/makingthings/types.h \
-  C:/mtcode/make/firmware/test/../controller/makingthings/osc.h
