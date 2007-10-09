46f82ec93e7b43f890da014349ec5d3b8d88c176
liamstask <liamstask@388e45b6-652a-11de-9f54-6d4fae1110e3>
2007-10-09 01:26:19 +0000

    - updated tiny and solo projects - removed stepper.h reference from controller.h

--- firmware/trunk/tiny/config.h
+++ firmware/trunk/tiny/config.h
@@ -1,59 +1,52 @@
 /*
-	config.h
-
-  Select which features & hardware you're using.
-
+	config.h - Select which features & hardware you're using.
   MakingThings
 */
 
 #ifndef CONFIG_H
 #define CONFIG_H
 
-/* 
-   Subsystems can be included or excluded here
- */
-//#define OSC
-
 #include "controller.h"   // ...everybody uses the MAKE Controller Board
+#include "error.h"
 
-#define CONTROLLER_HEAPSIZE 16000
+#define FIRMWARE_NAME          "Tiny-svn"
+#define FIRMWARE_MAJOR_VERSION 1
+#define FIRMWARE_MINOR_VERSION 2
+#define FIRMWARE_BUILD_NUMBER  1
 
-/********************************************************
-  Uncomment the revision of the MAKE Controller Board you're using.
-*********************************************************/
-//#define CONTROLLER_VERSION    50    // Atmel SAM7X eval. board
-//#define CONTROLLER_VERSION    90     
+
+#define CONTROLLER_HEAPSIZE 18200
+
+//----------------------------------------------------------------
+//  Comment out the systems that you don't want to include in your build.
+//----------------------------------------------------------------
+//#define MAKE_CTRL_USB     // enable the USB system
+//#define MAKE_CTRL_NETWORK // enable the Ethernet system
+//#define OSC               // enable the OSC system
+
+// ---------------------------------------------------------------
+// Network options
+// ---------------------------------------------------------------
+#define NETWORK_MEM_POOL         2000 // the network system's memory pool
+#define NETWORK_UDP_CONNS        4    // the number of possible DatagramSocket instances
+#define NETWORK_TCP_CONNS        4    // the number of possible Socket instances
+#define NETWORK_TCP_LISTEN_CONNS 2    // the number of possible ServerSocket instances
+
+//----------------------------------------------------------------
+//  Uncomment the revision of the MAKE Controller Board you're using.
+//----------------------------------------------------------------
+//#define CONTROLLER_VERSION  50    // Atmel SAM7X eval. board
+//#define CONTROLLER_VERSION  90     
 //#define CONTROLLER_VERSION  95       
 #define CONTROLLER_VERSION  100        
 
-// Error message defines
-#define CONTROLLER_OK                               0
-#define CONTROLLER_ERROR_CANT_LOCK                  1
-#define CONTROLLER_ERROR_TOO_MANY_USERS             2
-#define CONTROLLER_ERROR_TOO_MANY_STOPS             3
-#define CONTROLLER_ERROR_NOT_LOCKED                 4
-#define CONTROLLER_ERROR_ILLEGAL_INDEX              5
-#define CONTROLLER_ERROR_ILLEGAL_ID                 6
-#define CONTROLLER_ERROR_ILLEGAL_PARAMETER_VALUE    7
-#define CONTROLLER_ERROR_NOT_OPEN                   8
-#define CONTROLLER_ERROR_INSUFFICIENT_RESOURCES     9
-#define CONTROLLER_ERROR_BAD_DATA                   10
-#define CONTROLLER_ERROR_NO_SPACE                   11
-#define CONTROLLER_ERROR_RESOURCE_MISSING           12
-#define CONTROLLER_ERROR_NO_ADDRESS                 13
-#define CONTROLLER_ERROR_TIMEOUT                    14
-#define CONTROLLER_ERROR_BAD_ADDRESS                15
+//----------------------------------------------------------------
+//  Uncomment the revision of the MAKE Application Board you're using.
+//----------------------------------------------------------------
+//#define APPBOARD_VERSION  50    // Atmel SAM7X eval. board
+//#define APPBOARD_VERSION  90   
+//#define APPBOARD_VERSION  95    
+#define APPBOARD_VERSION  100   
 
-#define CONTROLLER_ERROR_COUNT_MISMATCH             100
-#define CONTROLLER_ERROR_START_FAILED               101
-#define CONTROLLER_ERROR_STOP_FAILED                102
-#define CONTROLLER_ERROR_WRONG_USER_COUNT           103
-#define CONTROLLER_ERROR_DATA_STRUCTURE_SIZE_WRONG  104
-#define CONTROLLER_ERROR_INCORRECT_INIT             105
-#define CONTROLLER_ERROR_INCORRECT_DEINIT           106
-#define CONTROLLER_ERROR_LOCK_ERROR                 107
-#define CONTROLLER_ERROR_LOCK_GRANTED_ERROR         108
-#define CONTROLLER_ERROR_USE_GRANTED_ERROR          109
-#define CONTROLLER_ERROR_INITIALIZATION             110
+#endif // CONFIG_H
 
-#endif /* CONFIG_H */
