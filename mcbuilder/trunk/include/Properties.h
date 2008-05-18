44e45badae8747ae44a331f9f0812c03f1dbd471
Liam Staskawicz <liam@stask.net>
2008-05-18 18:50:51 +0000

    - more mods to Properties

--- mcbuilder/trunk/include/Properties.h
+++ mcbuilder/trunk/include/Properties.h
@@ -13,10 +13,10 @@ class Properties : public QDialog, private Ui::Properties
 	Q_OBJECT
 	public:
 		Properties(MainWindow *mainWindow);
-    QString optLevel();
-    bool debug();
-    int heapsize();
     QString version() { return versionEdit->text(); }
+    QString optLevel() { return optLevelBox->currentText(); }
+    bool debug() { return (debugInfoCheckbox->checkState() == Qt::Checked); }
+    int heapsize() { return heapSizeEdit->text().toInt(); }
     bool includeOsc() { return (oscBox->checkState() == Qt::Checked); }
     bool includeUsb() { return (usbBox->checkState() == Qt::Checked); }
     bool includeNetwork() { return (networkBox->checkState() == Qt::Checked); }
@@ -27,14 +27,18 @@ class Properties : public QDialog, private Ui::Properties
     
 	public slots:
 		bool loadAndShow();
+    
 	private:
 		MainWindow *mainWindow;
 		QString propFilePath( );
     bool load();
     bool configChanged;
+    void setNetworkSectionEnabled(bool state);
+    
 	private slots:
 		void applyChanges( );
-
+    void restoreDefaults( );
+    void onNetworkChanged(int state);
 };
 
 #endif // PROPERTIES_H
