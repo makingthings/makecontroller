48075f9c125b0d9886fe69c1796094650f762cd2
liamstask <liamstask@388e45b6-652a-11de-9f54-6d4fae1110e3>
2008-06-20 19:28:24 +0000

    - first pass at JSON parsing - more doc

--- firmware/trunk/libraries/json/json.c
+++ firmware/trunk/libraries/json/json.c
@@ -18,6 +18,8 @@
 #include "json.h"
 #include "string.h"
 #include "stdio.h"
+#include <stdlib.h>
+#include <ctype.h>
 
 #define JSON_MAX_DEPTH 32
 
@@ -39,10 +41,22 @@ struct Json_t
 } Json;
 
 /** \defgroup JSON JSON
-	The Make Controller JSON library provides a simple, and not particularly full featured, library for parsing and 
-  generating JSON.  From http://www.json.org: "JSON (JavaScript Object Notation) is a lightweight 
-  data-interchange format. It is easy for humans to read and write. It is easy for machines 
-  to parse and generate."
+	The Make Controller JSON library provides a very small and very fast library for parsing and 
+  generating json. 
+  
+  From http://www.json.org: "JSON (JavaScript Object Notation) 
+  is a lightweight data-interchange format. It is easy for humans to read and write. It is easy for 
+  machines to parse and generate."
+
+  JSON is pretty widely used when communicating with web servers, or other network enabled devices.
+  It's nice and small, and easy to work with.  It's quite well supported in many programming
+  environments, so it's not a bad option for a communication format when you need to talk to other
+  devices from the Make Controller.  
+  
+  \b Disclaimer - in an attempt to keep it as small and as simple as possible, this library is not 
+  completely full featured at the moment.  It doesn't deal with escaped strings or some of the 
+  more exotic numeric representations outlined in the JSON specification.  It does, however, work
+  quite well for most other JSON tasks.
 
   \par Generating
   Generating JSON is pretty simple - just make successive calls to the API to add the desired
@@ -64,12 +78,47 @@ struct Json_t
   int json_len = MAX_JSON_LEN - remaining; // this is the total length of the string in jsonbuf
   \endcode
 
-  Note that calls to JsonEncode_String(), JsonEncode_Int() and JsonEncode_Bool() will add the
-  appropriate separators (: or , usually) to the string, depending on the context of the objects 
-  and arrays you've opened, or other data you've inserted.
+  Note that the library will add the appropriate separators (: or , usually) to the string, 
+  depending on the context of the objects and arrays you've opened, or other data you've inserted.
 
   \par Parsing
-  Not implemented yet...
+  Parsing is done using an event-based mechanism.  This means you can register for any events you care
+  to hear about, and then be called back with their value as they're encountered in the JSON string.
+
+  If you need to pass around some context that you would like available in each of the callbacks, 
+  you can pass it to JsonDecode() and it will be passed to each of the callbacks you've registered.
+  Otherwise, just pass 0 if you don't need it.
+
+  \code
+  // first, define the functions that we want to be called back on
+  bool on_obj_opened(void* ctx)
+  {
+    // deal with an object being opened...
+    return true;
+  }
+  
+  bool on_int(void *ctx, int val)
+  {
+    iny my_json_int = val;
+    // deal with a new int...
+    return true;
+  }
+  
+  bool on_string(void *ctx, char *string, int len)
+  {
+    // deal with a new string...
+    return true;
+  }
+
+  // Now, register these callbacks with the JSON parser.
+  JsonDecode_SetStartObjCallback(on_obj_opened);
+  JsonDecode_SetIntCallback(on_int);
+  JsonDecode_SetStringCallback(on_string);
+
+  // Finally, run the parser.
+  char jsonstr[] = "[{\"label\":\"value\",\"label2\":{\"nested\":234}}]";
+  JsonDecode(jsonstr, strlen(jsonstr), 0);
+  \endcode
 
   Thanks to YAJL (http://code.google.com/p/yajl-c) for some design inspiration.
   \par
@@ -83,7 +132,7 @@ struct Json_t
   Generally do this once you've successfully generated a string,
   and want to start on the next one.
 */
-void Json_Reset( )
+void JsonEncode_Reset( )
 {
   Json.encode_depth = 0;
   Json.states[0] = JSON_START;
@@ -326,8 +375,8 @@ char* JsonEncode_Int(char *buf, int value, int *remaining)
         return NULL;
       char temp[int_len+1];
       snprintf(temp, int_len+1, ":%d", value);
-      memcpy(buf, temp, int_len);
       int_len = strlen(temp);
+      memcpy(buf, temp, int_len);
       break;
     }
     default:
@@ -392,6 +441,7 @@ char* JsonEncode_Bool(char *buf, bool value, int *remaining)
   Called after adding a new value (atom) to a string in order
   to update the state appropriately.
 */
+// static
 void JsonEncode_AppendedAtom()
 {
   switch(Json.states[Json.encode_depth])
@@ -411,6 +461,434 @@ void JsonEncode_AppendedAtom()
   }
 }
 
+/****************************************************************************
+ JsonDecode
+****************************************************************************/
+
+typedef enum {
+    token_true,
+    token_false,
+    token_colon,
+    token_comma,     
+    token_eof,
+    token_error,
+    token_left_brace,     
+    token_left_bracket,
+    token_null,         
+    token_right_brace,     
+    token_right_bracket,
+    token_number, 
+    token_maybe_negative,
+    token_string,
+    token_unknown
+} JsonDecode_Token;
+
+struct Json_Callbacks_t
+{
+  bool(*null_callback)(void *ctx);
+  bool(*bool_callback)(void*, bool);
+  bool(*int_callback)(void*, int);
+  bool(*float_callback)(void*, float);
+  bool(*string_callback )(void*, char*, int);
+  bool(*start_obj_callback )(void*);
+  bool(*obj_key_callback )(void*, char*, int);
+  bool(*end_obj_callback )(void*);
+  bool(*start_array_callback )(void *ctx);
+  bool(*end_array_callback )(void *ctx);
+} Json_Callbacks;
+
+static JsonDecode_Token JsonDecode_GetToken(char* text, int len);
+
+/**
+  Set the function to be called back when an integer is parsed.
+  The function must have the format:
+  \code
+  bool on_int(void* context, int value);
+  \endcode
+  which would then be registered like so:
+  \code
+  JsonDecode_SetIntCallback(on_int);
+  \endcode
+  The \b context parameter can be optionally passed to JsonDecode() 
+  to provide context within the callback.
+
+  @param int_callback The function to be called back.
+*/
+void JsonDecode_SetIntCallback(bool(*int_callback)(void *ctx, int val))
+{
+  Json_Callbacks.int_callback = int_callback;
+}
+
+/**
+  Set the function to be called back when a float is parsed.
+  The function must have the format:
+  \code
+  bool on_float(void* context, float value);
+  \endcode
+  which would then be registered like so:
+  \code
+  JsonDecode_SetFloatCallback(on_float);
+  \endcode
+  The \b context parameter can be optionally passed to JsonDecode() 
+  to provide context within the callback.
+
+  @param float_callback The function to be called back.
+*/
+void JsonDecode_SetFloatCallback(bool(*float_callback)(void *ctx, float val))
+{
+  Json_Callbacks.float_callback = float_callback;
+}
+
+/**
+  Set the function to be called back when a boolean is parsed.
+  The function must have the format:
+  \code
+  bool on_bool(void* context, bool value);
+  \endcode
+  which would then be registered like so:
+  \code
+  JsonDecode_SetBoolCallback(on_bool);
+  \endcode
+  The \b context parameter can be optionally passed to JsonDecode() 
+  to provide context within the callback.
+
+  @param bool_callback The function to be called back.
+*/
+void JsonDecode_SetBoolCallback(bool(*bool_callback)(void *ctx, bool val))
+{
+  Json_Callbacks.bool_callback = bool_callback;
+}
+
+/**
+  Set the function to be called back when a string is parsed.
+  The function must have the format:
+  \code
+  bool on_string(void* context, char* string);
+  \endcode
+  which would then be registered like so:
+  \code
+  JsonDecode_SetStringCallback(on_string);
+  \endcode
+  The \b context parameter can be optionally passed to JsonDecode() 
+  to provide context within the callback.
+
+  @param string_callback The function to be called back.
+*/
+void JsonDecode_SetStringCallback(bool(*string_callback)(void *ctx, char *string, int len))
+{
+  Json_Callbacks.string_callback = string_callback;
+}
+
+/**
+  Set the function to be called back when an object is started.
+  The left bracket - { - is the opening element of an object.
+  The function must have the format:
+  \code
+  bool on_obj_started(void* context);
+  \endcode
+  which would then be registered like so:
+  \code
+  JsonDecode_SetStartObjCallback(on_obj_started);
+  \endcode
+  The \b context parameter can be optionally passed to JsonDecode() 
+  to provide context within the callback.
+
+  @param start_obj_callback The function to be called back.
+*/
+void JsonDecode_SetStartObjCallback(bool(*start_obj_callback)(void *ctx))
+{
+  Json_Callbacks.start_obj_callback = start_obj_callback;
+}
+
+void JsonDecode_SetObjKeyCallback(bool(*obj_key_callback)(void *ctx, char *key, int len))
+{
+  Json_Callbacks.obj_key_callback = obj_key_callback;
+}
+
+/**
+  Set the function to be called back when an object is ended.
+  The right bracket - } - is the closing element of an object.
+  The function must have the format:
+  \code
+  bool on_obj_ended(void* context);
+  \endcode
+  which would then be registered like so:
+  \code
+  JsonDecode_SetEndObjCallback(on_obj_ended);
+  \endcode
+  The \b context parameter can be optionally passed to JsonDecode() 
+  to provide context within the callback.
+
+  @param end_obj_callback The function to be called back.
+*/
+void JsonDecode_SetEndObjCallback(bool(*end_obj_callback)(void *ctx))
+{
+  Json_Callbacks.end_obj_callback = end_obj_callback;
+}
+
+/**
+  Set the function to be called back when an array is started.
+  The left brace - [ - is the starting element of an array.
+  The function must have the format:
+  \code
+  bool on_array_started(void* context);
+  \endcode
+  which would then be registered like so:
+  \code
+  JsonDecode_SetStartArrayCallback(on_array_started);
+  \endcode
+  The \b context parameter can be optionally passed to JsonDecode() 
+  to provide context within the callback.
+
+  @param start_array_callback The function to be called back.
+*/
+void JsonDecode_SetStartArrayCallback(bool(*start_array_callback)(void *ctx))
+{
+  Json_Callbacks.start_array_callback = start_array_callback;
+}
+
+/**
+  Set the function to be called back when an array is ended.
+  The right brace - ] - is the closing element of an array.
+  The function must have the format:
+  \code
+  bool on_array_ended(void* context);
+  \endcode
+  which would then be registered like so:
+  \code
+  JsonDecode_SetEndArrayCallback(on_array_ended);
+  \endcode
+  The \b context parameter can be optionally passed to JsonDecode() 
+  to provide context within the callback.
+
+  @param end_array_callback The function to be called back.
+*/
+void JsonDecode_SetEndArrayCallback(bool(*end_array_callback)(void *ctx))
+{
+  Json_Callbacks.end_array_callback = end_array_callback;
+}
+
+/**
+  Parse a JSON string.
+  The JSON parser is event based, meaning that you will receive any callbacks
+  you registered for as the elements are encountered in the JSON string.
+
+  @param text The JSON string to parse.
+  @param len The length of the JSON string.
+  @param context An optional paramter that your code can use to 
+  pass around a known object within the callbacks.  Otherwise, just set it to 0
+  @return True on successful parse, false on failure.
+
+  \par Example
+  \code
+  // quotes are escaped since I'm writing it out manually
+  char jsonstr[] = "[{\"label\":\"value\",\"label2\":{\"nested\":234}}]";
+  JsonDecode(jsonstr, strlen(jsonstr), 0); // don't pass in any context
+  // now we expect to be called back on any callbacks we registered.
+  \endcode
+*/
+bool JsonDecode(char* text, int len, void* context)
+{
+  JsonDecode_Token token;
+  while(len)
+  {
+    while(*text == ' ') // eat white space
+      text++;
+    token = JsonDecode_GetToken( text, len);
+    switch(token)
+    {
+      case token_true:
+        if(Json_Callbacks.bool_callback)
+        {
+          if(!Json_Callbacks.bool_callback(context, true))
+            return false;
+        }
+        text += 4;
+        len -= 4;
+        break;
+      case token_false:
+        if(Json_Callbacks.bool_callback)
+        {
+          if(!Json_Callbacks.bool_callback(context, false))
+            return false;
+        }
+        text += 5;
+        len -= 5;
+        break;
+      case token_null:
+        if(Json_Callbacks.null_callback)
+        {
+          if(!Json_Callbacks.null_callback(context))
+            return false;
+        }
+        text += 4;
+        len -= 4;
+        break;
+      case token_colon: // just get the next one
+      case token_comma:
+        text++;
+        len--;
+        break;
+      case token_left_bracket:
+        if(Json_Callbacks.start_obj_callback)
+        {
+          if(!Json_Callbacks.start_obj_callback(context))
+            return false;
+        }
+        text++;
+        len--;
+        break;
+      case token_right_bracket:
+        if(Json_Callbacks.end_obj_callback)
+        {
+          if(!Json_Callbacks.end_obj_callback(context))
+            return false;
+        }
+        text++;
+        len--;
+        break;
+      case token_left_brace:
+        if(Json_Callbacks.start_array_callback)
+        {
+          if(!Json_Callbacks.start_array_callback(context))
+            return false;
+        }
+        text++;
+        len--;
+        break;
+      case token_right_brace:
+        if(Json_Callbacks.end_array_callback)
+        {
+          if(!Json_Callbacks.end_array_callback(context))
+            return false;
+        }
+        text++;
+        len--;
+        break;
+      case token_number:
+      {
+        const char* p = text;
+        bool keepgoing = true;
+        bool gotdecimal = false;
+        do
+        {
+          if(*p == '.')
+          {
+            if(gotdecimal) // we only expect to get one decimal place in a number
+              return false;
+            gotdecimal = true;
+            p++;
+          }
+          else if(!isdigit(*p))
+            keepgoing = false;
+          else
+            p++;
+        } while(keepgoing);
+        int size = p - text;
+        if(gotdecimal)
+        {
+          if(Json_Callbacks.float_callback)
+          {
+            if(!Json_Callbacks.float_callback(context, atof(text)))
+              return false;
+          }
+        }
+        else
+        {
+          if(Json_Callbacks.int_callback)
+          {
+            if(!Json_Callbacks.int_callback(context, atoi(text)))
+              return false;
+          }
+        }
+        text += size;
+        len -= size;
+        break;
+      }
+      case token_string:
+      {
+        char* p = ++text;
+        len--;
+        while(*p != '"') // just go until the next " for now...may ultimately check for escaped data
+          p++;
+        int size = p - text;
+        *p = 0; // replace the trailing " with a null to make a string
+        if(Json_Callbacks.string_callback)
+        {
+          if(!Json_Callbacks.string_callback(context, text, size))
+            return false;
+        }
+        text += (size+1); // account for the trailing "
+        len -= (size+1);
+        break;
+      }
+      case token_eof: // we don't expect to get this, since our len should run out before we get eof
+        return false;
+      default:
+        return false;
+    }
+  }
+  return true;
+}
+
+// static
+JsonDecode_Token JsonDecode_GetToken(char* text, int len)
+{
+  char c = text[0];
+  switch(c)
+  {
+    case ':':
+      return token_colon;
+    case ',':
+      return token_comma;
+    case '{':
+      return token_left_bracket;
+    case '}':
+      return token_right_bracket;
+    case '[':
+      return token_left_brace;
+    case ']':
+      return token_right_brace;
+    case '"':
+      return token_string;
+    case '0': case '1': case '2': case '3': case '4': 
+    case '5': case '6': case '7': case '8': case '9':
+      return token_number;
+    case '-':
+      return token_maybe_negative;
+    case 't':
+    {
+      if(len < 4 + 1) // not enough space;
+        return token_unknown;
+      if(!strncmp(text, "true", 4))
+        return token_true;
+      else 
+        return token_unknown;
+    }
+    case 'f':
+    {
+      if(len < 5 + 1) // not enough space;
+        return token_unknown;
+      if(!strncmp(text, "false", 5))
+        return token_false;
+      else
+        return token_unknown;
+    }
+    case 'n':
+    {
+      if(len < 4 + 1) // not enough space;
+        return token_unknown;
+      if(!strncmp(text, "null", 4))
+        return token_null;
+      else
+        return token_unknown;
+    }
+    case '\0':
+      return token_eof;
+    default:
+      return token_unknown;
+  }
+}
 
 
 
