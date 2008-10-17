612181062325fbb4d94787e84755bad545afb347
Liam Staskawicz <liam@stask.net>
2008-10-17 21:18:33 +0000

    - switch to .txt for src ReadMe - add src package creation to release script

--- maxmsp/mc_usb/trunk/release-osx.sh
+++ maxmsp/mc_usb/trunk/release-osx.sh
@@ -10,6 +10,7 @@ echo Creating mcbuilder $VERSION OS X distribution...
 # remove any previous copies
 rm -Rf mc.usb-$VERSION
 rm -Rf *.dmg
+rm -Rf *.zip
 
 # put it in the right spot and create the appropriate directories
 mkdir mc.usb-$VERSION
@@ -30,6 +31,23 @@ find mc.usb-$VERSION -name ".svn" -exec rm -rf {} ';' 2> /dev/null
 echo creating .dmg...
 hdiutil create -fs HFS+ -srcfolder "./mc.usb-$VERSION/" -volname "mc.usb-$VERSION" "mc.usb-$VERSION.dmg"
 
+# create the source release
+echo Creating source package
+mkdir mc.usb-src
+cp -r src mc.usb-src
+cp -R mc.usb.xcodeproj mc.usb-src
+rm mc.usb-src/mc.usb.xcodeproj/*.pbxuser
+rm mc.usb-src/mc.usb.xcodeproj/*.mode*
+cp ReadMe-dev.txt mc.usb-src/ReadMe.txt
+# remove any crap
+find mc.usb-src -name "*~" -exec rm -f {} ';'
+find mc.usb-src -name "._*" -exec rm -f {} ';'
+find mc.usb-src -name "Thumbs.db" -exec rm -f {} ';'
+find mc.usb-src -name ".svn" -exec rm -rf {} ';' 2> /dev/null
+
+zip -rq mc.usb-src-$VERSION.zip mc.usb-src 
+
 rm -Rf mc.usb-$VERSION
+rm -Rf mc.usb-src
 
 echo Done.
