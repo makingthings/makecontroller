2c2010182bb2d50cd99683ade229caaf5dc8c95f
liamMT <liamMT@388e45b6-652a-11de-9f54-6d4fae1110e3>
2008-10-17 20:48:14 +0000

    add release script for os x

---
+++ maxmsp/mc_usb/trunk/release-osx.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# script for generating an mc.usb release on OS X
+
+#grab the version
+VERSION=v0.5
+
+echo Creating mcbuilder $VERSION OS X distribution...
+
+# remove any previous copies
+rm -Rf mc.usb-$VERSION
+rm -Rf *.dmg
+
+# put it in the right spot and create the appropriate directories
+mkdir mc.usb-$VERSION
+cp ReadMe-OSX.rtf mc.usb-$VERSION/ReadMe.rtf
+mkdir mc.usb-$VERSION/MakeController-externals
+mkdir mc.usb-$VERSION/MakeController-help
+cp -r MakeController-externals mc.usb-$VERSION
+cp -r MakeController-help mc.usb-$VERSION
+cp mc-objectlist.txt mc.usb-$VERSION
+
+# remove any crap
+find mc.usb-$VERSION -name "*~" -exec rm -f {} ';'
+find mc.usb-$VERSION -name "._*" -exec rm -f {} ';'
+find mc.usb-$VERSION -name "Thumbs.db" -exec rm -f {} ';'
+find mc.usb-$VERSION -name ".svn" -exec rm -rf {} ';' 2> /dev/null
+
+# create the .dmg
+echo creating .dmg...
+hdiutil create -fs HFS+ -srcfolder "./mc.usb-$VERSION/" -volname "mc.usb-$VERSION" "mc.usb-$VERSION.dmg"
+
+rm -Rf mc.usb-$VERSION
+
+echo Done.
