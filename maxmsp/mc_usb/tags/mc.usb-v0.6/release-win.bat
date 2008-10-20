d4d983834361a4ac7cacff1c3bf6dfbad0c49e68
Liam Staskawicz <liam@stask.net>
2008-10-20 23:20:07 +0000

    tagging v0.6

---
+++ maxmsp/mc_usb/tags/mc.usb-v0.6/release-win.bat
@@ -0,0 +1,12 @@
+set VERSION=v0.6
+
+rmdir mc.usb-win-%VERSION%
+
+mkdir mc.usb-win-%VERSION%
+mkdir mc.usb-win-%VERSION%\MakeController-externals
+mkdir mc.usb-win-%VERSION%\MakeController-help
+copy MakeController-externals mc.usb-win-%VERSION%\MakeController-externals
+copy msvc\Release\mc.usb.mxe mc.usb-win-%VERSION%\MakeController-externals
+copy MakeController-help mc.usb-win-%VERSION%\MakeController-help
+copy mc-objectlist.txt mc.usb-win-%VERSION%
+copy ReadMe-WIN.rtf mc.usb-win-%VERSION%\ReadMe.rtf
