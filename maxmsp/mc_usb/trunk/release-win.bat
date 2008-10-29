c1fd3ea74e331a0d2ff6fd05bde44ca34014dba9
liamMT <liamMT@388e45b6-652a-11de-9f54-6d4fae1110e3>
2008-10-29 17:00:51 +0000

    - update to package max4 abstractions and help files

--- maxmsp/mc_usb/trunk/release-win.bat
+++ maxmsp/mc_usb/trunk/release-win.bat
@@ -1,12 +1,16 @@
-set VERSION=v0.6
+set VERSION=v0.6.1
 
 rmdir mc.usb-win-%VERSION%
 
 mkdir mc.usb-win-%VERSION%
 mkdir mc.usb-win-%VERSION%\MakeController-externals
 mkdir mc.usb-win-%VERSION%\MakeController-help
+mkdir mc.usb-win-%VERSION%\max4
+xcopy max4 mc.usb-win-%VERSION%\max4 /E
 copy MakeController-externals mc.usb-win-%VERSION%\MakeController-externals
+delete mc.usb-win-%VERSION%\MakeController-externals\*.mxo
 copy msvc\Release\mc.usb.mxe mc.usb-win-%VERSION%\MakeController-externals
+copy mc.usb-win-%VERSION%\MakeController-externals\*.mxe mc.usb-win-%VERSION%\max4\MakeController-externals
 copy MakeController-help mc.usb-win-%VERSION%\MakeController-help
 copy mc-objectlist.txt mc.usb-win-%VERSION%
 copy ReadMe-WIN.rtf mc.usb-win-%VERSION%\ReadMe.rtf
