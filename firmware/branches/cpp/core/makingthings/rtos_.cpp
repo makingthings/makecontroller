689c31f673c269a5daf16be599a92e4d673ea408
Liam Staskawicz <liam@stask.net>
2008-11-18 16:39:36 +0000

    - redefine standard malloc and free instead of creating separate functions for them - stub out RTOS c++ stuff

---
+++ firmware/branches/cpp/core/makingthings/rtos_.cpp
@@ -0,0 +1,211 @@
+
+#include "rtos_.h"
+
+
+/**********************************************************************************
+
+                                  Task
+                                  
+**********************************************************************************/
+
+Task::Task( void (loop)(void*), const char* name, int stackDepth, void* parameters, int priority )
+{
+  if( xTaskCreate( loop, (const signed char*)name, ( stackDepth >> 2 ), parameters, priority, &_task ) != 1 )
+    _task = NULL;
+  return;
+}
+
+Task::~Task( )
+{
+  vTaskDelete( _task );
+}
+
+int Task::getRemainingStack( )
+{
+  if( !_task )
+    return -1;
+  else
+    return usVoidTaskCheckFreeStackSpace( _task );
+}
+
+// Task* RTOS::getTaskByName( char *taskName )
+// {
+//   void *tcb = NULL;
+//   vTaskSuspendAll();
+//   {
+//     tcb = findTask( taskName, -1 );
+//   }
+//   xTaskResumeAll();
+//   return tcb;
+// }
+
+int Task::getPriority( )
+{
+  return xTaskGetPriority( _task );
+}
+
+void Task::setPriority( int priority )
+{
+  vTaskPrioritySet( _task, priority );
+}
+
+int Task::getIDNumber( )
+{
+  return xTaskGetIDNumber( _task );
+}
+
+char* Task::getName( )
+{
+  return (char*)xTaskGetName( _task );
+}
+
+int Task::getStackAllocated( )
+{
+  return xTaskGetStackAllocated( _task );
+}
+
+Task* Task::getNext( )
+{
+  // void* taskreturn = NULL;
+  // vTaskSuspendAll();
+  // {
+  //   taskreturn = TaskGetNext_internal( task );
+  // }
+  // xTaskResumeAll( );
+  // return taskreturn;
+}
+
+/**********************************************************************************
+
+                                  RTOS
+                                  
+**********************************************************************************/
+
+Task* RTOS::getTaskByName( const char* name ) // static
+{
+  // void *tcb = NULL;
+  // vTaskSuspendAll();
+  // {
+  //   tcb = findTask( taskName, -1 );
+  // }
+  // xTaskResumeAll();
+  // return tcb;
+}
+
+Task* RTOS::getTaskByID( int id ) // static
+{
+  // void* tcb = NULL;
+  // vTaskSuspendAll();
+  // {
+  //   tcb = findTask( NULL, taskID );
+  // }
+  // xTaskResumeAll();
+  // return tcb;
+}
+
+Task* RTOS::getCurrentTask( ) // static
+{
+  // return xTaskGetCurrentTaskHandle( );
+}
+
+int RTOS::numberOfTasks( ) // static
+{
+  return uxTaskGetNumberOfTasks( );
+}
+
+int RTOS::topTaskPriority( ) // static
+{
+  return xTaskGetTopUsedPriority( );
+}
+
+int RTOS::ticksSinceBoot( ) // static
+{
+  return xTaskGetTickCount( );
+}
+
+/**********************************************************************************
+
+                                  Queue
+                                  
+**********************************************************************************/
+
+Queue::Queue( uint length, uint itemSize )
+{
+  _q = xQueueCreate( length, itemSize );
+}
+
+Queue::~Queue( )
+{
+  vQueueDelete( _q );
+}
+
+int Queue::sendToFront( void* itemToQueue, int timeout )
+{
+  return xQueueSend( _q, itemToQueue, timeout / portTICK_RATE_MS );
+}
+
+int Queue::sendToBack( void* itemToQueue, int timeout )
+{
+  return xQueueSend( _q, itemToQueue, timeout / portTICK_RATE_MS );
+}
+
+int Queue::receive( void* buffer, int timeout )
+{
+  return xQueueReceive( _q, buffer, timeout / portTICK_RATE_MS );
+}
+
+int Queue::msgsAvailable( )
+{
+  return uxQueueMessagesWaiting( _q );
+}
+
+int Queue::sendToFrontFromISR( void* itemToSend, int taskPreviouslyWoken )
+{
+  return xQueueSendToFrontFromISR( _q, itemToSend, taskPreviouslyWoken );
+}
+
+int Queue::sendToBackFromISR( void* itemToSend, int taskPreviouslyWoken )
+{
+  return xQueueSendToBackFromISR( _q, itemToSend, taskPreviouslyWoken );
+}
+
+int Queue::receiveFromISR( void* buffer, long* taskWoken )
+{
+  return xQueueReceiveFromISR( _q, buffer, taskWoken );
+}
+
+/**********************************************************************************
+
+                                  Semaphore
+                                  
+**********************************************************************************/
+
+Semaphore::Semaphore( )
+{
+  vSemaphoreCreateBinary( _sem );
+}
+
+Semaphore::~Semaphore( )
+{
+  delete _sem;
+}
+
+int Semaphore::take( int timeout )
+{
+  return xSemaphoreTake( _sem, timeout / portTICK_RATE_MS );
+}
+
+int Semaphore::give( )
+{
+  return xSemaphoreGive( _sem );
+}
+
+int Semaphore::giveFromISR( int taskPreviouslyWoken )
+{
+  return xSemaphoreGiveFromISR( _sem, taskPreviouslyWoken );
+}
+
+
+
+
+
---
+++ firmware/branches/cpp/core/makingthings/rtos_.h
@@ -0,0 +1,91 @@
+/*********************************************************************************
+
+ Copyright 2006-2008 MakingThings
+
+ Licensed under the Apache License, 
+ Version 2.0 (the "License"); you may not use this file except in compliance 
+ with the License. You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0 
+ 
+ Unless required by applicable law or agreed to in writing, software distributed
+ under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ CONDITIONS OF ANY KIND, either express or implied. See the License for
+ the specific language governing permissions and limitations under the License.
+
+*********************************************************************************/
+
+#ifndef RTOS__H
+#define RTOS__H
+
+extern "C" {
+  #include "projdefs.h"
+  #include "portmacro.h"
+  #include "queue.h"
+  #include "FreeRTOS.h"
+  #include "task.h"
+  #include "semphr.h"
+}
+
+void  Sleep( int timems );
+void  TaskYield( void );
+void  TaskEnterCritical( void );
+void  TaskExitCritical( void );
+
+class Task
+{
+public:
+  Task( void (loop)(void*), const char* name, int stackDepth, void* parameters, int priority );
+  ~Task( );
+  int getRemainingStack( );
+  int getPriority( );
+  void setPriority( int priority );
+  int getIDNumber( );
+  char* getName( );
+  int getStackAllocated( );
+  Task* getNext( );
+private:
+  void* _task;
+  // Task( void* task ) { _task = task; }
+};
+
+class RTOS
+{
+public:
+  static Task* getTaskByName( const char* name );
+  static Task* getTaskByID( int id );
+  static Task* getCurrentTask( );
+  static int numberOfTasks( );
+  static int topTaskPriority( );
+  static int ticksSinceBoot( );
+};
+
+class Queue
+{
+public:
+  Queue( uint length, uint itemSize );
+  ~Queue( );
+  int sendToFront( void* itemToQueue, int timeout );
+  int sendToBack( void* itemToQueue, int timeout );
+  int receive( void* buffer, int timeout );
+  int msgsAvailable( );
+  int sendToFrontFromISR( void* itemToSend, int taskPreviouslyWoken );
+  int sendToBackFromISR( void* itemToSend, int taskPreviouslyWoken );
+  int receiveFromISR( void* buffer, long* taskWoken );
+private:
+  void* _q;
+};
+
+class Semaphore
+{
+public:
+  Semaphore( );
+  ~Semaphore( );
+  int take( int timeout );
+  int give( );
+  int giveFromISR( int taskPreviouslyWoken );
+private:
+  void* _sem;
+};
+
+#endif // RTOS__H
