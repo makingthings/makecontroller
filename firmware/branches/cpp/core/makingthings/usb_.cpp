4a60eb632812781af3503448a41fed4716186021
Liam Staskawicz <liam@stask.net>
2008-11-19 02:44:01 +0000

    stub of USB class

---
+++ firmware/branches/cpp/core/makingthings/usb_.cpp
@@ -0,0 +1,60 @@
+/*********************************************************************************
+
+ Copyright 2006-2008 MakingThings
+
+ Licensed under the Apache License, 
+ Version 2.0 (the "License"); you may not use this file except in compliance 
+ with the License. You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0 
+ 
+ Unless required by applicable law or agreed to in writing, software distributed
+ under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ CONDITIONS OF ANY KIND, either express or implied. See the License for
+ the specific language governing permissions and limitations under the License.
+
+*********************************************************************************/
+
+#include "usb_.h"
+extern "C" {
+  #include "CDCDSerialDriver.h"
+  #include "CDCDSerialDriverDescriptors.h"
+}
+
+#include "appled_cpp.h"
+
+/// Size in bytes of the buffer used for reading data from the USB & USART
+#define DATABUFFERSIZE BOARD_USB_ENDPOINTS_MAXPACKETSIZE(CDCDSerialDriverDescriptors_DATAIN)
+
+void USB::init( ) // static
+{
+  AppLed led(3);
+  led.setState(1);
+  CDCDSerialDriver_Initialize();
+  USBD_Connect();
+  while (USBD_GetState() < USBD_STATE_CONFIGURED); // wait for things to get set up
+  led.setState(0);
+}
+
+int USB::read( char *buffer, int length ) // static
+{
+  
+}
+
+int USB::write( char *buffer, int length ) // static
+{
+  
+}
+
+int USB::readSlip( char *buffer, int length ) // static
+{
+  
+}
+
+int USB::writeSlip( char *buffer, int length ) // static
+{
+  
+}
+
+
+
