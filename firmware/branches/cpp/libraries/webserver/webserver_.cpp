0db3ef66bd519d93d34ddb470dca8cd168429d23
liamMT <liamMT@388e45b6-652a-11de-9f54-6d4fae1110e3>
2008-12-11 23:16:40 +0000

    - more work on webserver

--- firmware/branches/cpp/libraries/webserver/webserver_.cpp
+++ firmware/branches/cpp/libraries/webserver/webserver_.cpp
@@ -17,6 +17,11 @@
 
 #include "webserver_.h"
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
 #define HTTP_OK "HTTP/1.0 200 OK\r\nContent-type: "
 #define HTTP_CONTENT_HTML "text/html\r\n\r\n"
 #define HTTP_CONTENT_PLAIN "text/plain\r\n\r\n"
@@ -39,6 +44,10 @@ WebServer* WebServer::get()
   return _instance;
 }
 
+/*
+  Wait for new connections (making sure we're listening on the right port)
+  and then dispatch them to processRequest()
+*/
 void webServerLoop(void* parameters)
 {
   WebServer* ws = (WebServer*)parameters;
@@ -55,31 +64,70 @@ void webServerLoop(void* parameters)
     if ( request != NULL )
     {
       ws->hits++;
-      ws->processRequest( request );
+      HttpMethod method;
+      request->readLine( ws->requestBuf, REQUEST_SIZE_MAX ); 
+      char* address = ws->getRequestAddress( ws->requestBuf, REQUEST_SIZE_MAX, &method );
+      ws->processRequest( request, method, address );
       delete request;
     }
     Task::sleep( 5 );
   }
 }
 
-void WebServer::processRequest( TcpSocket* request )
+/**
+  Handle a new web request.
+  If you want to create a new webserver, you can inherit from WebServer and re-implement
+  this function to provide new functionality.
+
+  \b Example
+  \code
+  class MyWebServer : public WebServer
+  {
+    // ... other class definitions here ...
+    void processRequest( TcpSocket* request, HttpMethod method, char* path );
+  };
+  \endcode
+*/
+void WebServer::processRequest( TcpSocket* request, HttpMethod method, char* path )
 {
-//  char* requestType = 0;
-  char* address = 0;
-  int i;
-  
-  request->readLine( requestBuf, REQUEST_SIZE_MAX ); 
-  //address = getRequestAddress( requestBuf, REQUEST_SIZE_MAX, &requestType );
-  
   bool responded = false;
   if ( handler_count )
   {
+    int i;
     for ( i = 0; i < handler_count; i++ )
     {
       WebHandler* hp = handlers[ i ];
-      if ( strncmp( hp->address(), address, strlen( hp->address() ) ) == 0 )
+      if ( strncmp( hp->address(), path, strlen( hp->address() ) ) == 0 )
       {
-        //responded = hp->newRequest( requestType, address, WebServer->request, REQUEST_SIZE_MAX, requestSocket, WebServer->response, RESPONSE_SIZE_MAX );
+        switch(method)
+        {
+          case HTTP_GET:
+            responded = hp->get(path);
+            break;
+          case HTTP_POST:
+          {
+            int path_len = strlen(path);
+            char temp_path[path_len+1];
+            strncpy( temp_path, path, path_len);
+            int content_len = getBody( request, requestBuf, REQUEST_SIZE_MAX );
+            responded = hp->post(temp_path, requestBuf, content_len);
+            break;
+          }
+          case HTTP_PUT:
+          {
+            int path_len = strlen(path);
+            char temp_path[path_len+1];
+            strncpy( temp_path, path, path_len);
+            int content_len = getBody( request, requestBuf, REQUEST_SIZE_MAX );
+            responded = hp->put(temp_path, requestBuf, content_len);
+            break;
+          }
+          case HTTP_DELETE:
+            responded = hp->del(path);
+            break;
+          default:
+            break;
+        }
         if ( responded )
           break;
       }
@@ -89,6 +137,96 @@ void WebServer::processRequest( TcpSocket* request )
 //    TestHandler(requestType, address, WebServer->request, REQUEST_SIZE_MAX, requestSocket, WebServer->response, RESPONSE_SIZE_MAX);
   
   request->close();
+}
+
+/*
+  Extract the HTTP method for this request, and then return a pointer to the beginning
+  of the URL path
+*/
+char* WebServer::getRequestAddress( char* request, int length, HttpMethod* method )
+{
+  char* last = request + length;
+  char* address = NULL;
+
+  // Skip any initial spaces
+  while( *request == ' ' )
+    request++;
+  if ( request > last ) // make sure we didn't go too far
+    return NULL;
+
+  if( !strncmp(request, "GET", 3) )
+    *method = HTTP_GET;
+  else if( !strncmp(request, "POST", 4) )
+    *method = HTTP_POST;
+  else if( !strncmp(request, "PUT", 3) )
+    *method = HTTP_PUT;
+  else if( !strncmp(request, "DELETE", 6) )
+    *method = HTTP_DELETE;
+
+  // Skip the request type
+  while ( *request != ' ' )
+    request++;
+
+  if ( request > last )
+    return address;
+
+  // Skip any subsequent spaces
+  while( *request == ' ' )
+    request++;
+
+  if ( request > last )
+    return address;
+
+  address = request;
+  
+  // now terminate the end of the address so we can do stringy things on it
+  while( !isspace( *request ) )
+    request++;
+  
+  if ( request < last )
+    *request = 0;
+
+  return address;
+}
+
+int WebServer::getBody( TcpSocket* socket, char* requestBuffer, int maxSize )
+{
+  // keep reading lines of the HTTP header until we get CRLF which signifies the end of the
+  // header and the start of the body data.  If we see the contentlength along the way, keep that.
+  int contentLength = 0;
+  int bufferLength = 0;
+  while ( ( bufferLength = socket->readLine( requestBuffer, maxSize ) ) )
+  {
+    if ( strncmp( requestBuffer, "\r\n", 2 ) == 0 )
+      break;
+    if ( strncmp( requestBuffer, "Content-Length", 14 ) == 0 )
+      contentLength = atoi( &requestBuffer[ 15 ] );
+  }
+  
+  // now we should be down to the HTTP POST data
+  // if there's any data, get up into it
+  int bufferRead = 0;
+  if ( contentLength > 0 && bufferLength > 0 )
+  {  
+    int lengthToRead;
+    int avail = socket->bytesAvailable();
+    if(avail > maxSize)
+      lengthToRead = maxSize - 1;
+    else
+      lengthToRead = avail;
+    char *rbp = requestBuffer;
+    // read all that the socket has to offer...may come in chunks, so keep going until there's none left
+    while ( ( bufferLength = socket->read( rbp, lengthToRead ) ) )
+    {
+      bufferRead += bufferLength;
+      rbp += bufferLength;
+      lengthToRead -= bufferLength;
+      if ( bufferRead >= contentLength )
+        break;
+    }
+    requestBuffer[ bufferRead ] = 0; // null-terminate the request
+  }
+  return bufferRead;
 }
 
 
