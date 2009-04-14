0ec27eb49035a822c8647d812ba847e6e79efacd
liamMT <liamMT@388e45b6-652a-11de-9f54-6d4fae1110e3>
2009-04-14 19:02:09 +0000

    - refactor motor library to c++...still needs more testing, etc. - add missing setAll() method to pwmout

--- firmware/branches/cpp/libraries/motor/motor.h
+++ firmware/branches/cpp/libraries/motor/motor.h
@@ -15,24 +15,51 @@
 
 *********************************************************************************/
 
-/*
-	motor.h
-
-  Includes, etc. so make users don't have to
-
-  MakingThings
-*/
-
 #ifndef MOTOR_H
 #define MOTOR_H
 
-int Motor_SetActive( int index, int state );
-int Motor_GetActive( int index );
+#include "pwmout.h"
 
-int Motor_SetSpeed( int motor, int duty );
-int Motor_SetDirection( int motor, char forward );
-int Motor_GetSpeed( int motor );
-int Motor_GetDirection( int motor );
+/**
+  The Motor subsystem provides forward/reverse and speed control for up to 4 DC motors across the 8 high current outputs.
+  Each motor controller is composed of 2 adjacent Digital Outs on the Make Application Board:
+  - motor 0 - Digital Outs 0 and 1.
+  - motor 1 - Digital Outs 2 and 3.
+  - motor 2 - Digital Outs 4 and 5.
+  - motor 3 - Digital Outs 6 and 7.
+  
+  Other output devices cannot be used simultaneously - for example, the DigitalOuts cannot be called without
+  first setting overlapping the DC motor I/O lines to inactive.
+  
+  See the digital out section of the 
+  <a href="http://www.makingthings.com/documentation/tutorial/application-board-overview/digital-outputs">
+  Application Board overview</a> for more details.
+  \ingroup Libraries
+*/
+class Motor
+{
+  public:
+    Motor(int index);
+    ~Motor();
+
+    int speed();
+    bool setSpeed(int speed);
+
+    bool direction();
+    bool setDirection(bool forward);
+
+  protected:
+    int _index;
+    typedef struct {
+      PwmOut* pwmout;
+      short refcount;
+      int direction;
+      int speed;
+    } MotorInternal;
+    static MotorInternal* motors[]; // only ever want to make 4 of these
+
+    void finalize(MotorInternal* m);
+};
 
 /* OSC Interface */
 const char* MotorOsc_GetName( void );
