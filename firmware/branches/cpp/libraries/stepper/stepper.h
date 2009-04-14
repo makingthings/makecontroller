aec9596fa1b6fe62060bd09a5e31168bd21627fb
Liam Staskawicz <liam@stask.net>
2009-04-14 21:21:56 +0000

    - refactor stepper to c++...needs more testing

--- firmware/branches/cpp/libraries/stepper/stepper.h
+++ firmware/branches/cpp/libraries/stepper/stepper.h
@@ -15,34 +15,94 @@
 
 *********************************************************************************/
 
-/*
-	stepper.h
-
-  MakingThings
-*/
 
 #ifndef STEPPER_H
 #define STEPPER_H
 
+#include "fasttimer.h"
+#include "pwm.h"
+#include "io_cpp.h"
 
-int Stepper_SetActive( int index, int state );
-int Stepper_GetActive( int index );
+#define STEPPER_COUNT 2
 
-int  Stepper_SetPosition( int index, int position );
-int  Stepper_GetPosition( int index );
-int  Stepper_SetPositionRequested( int index, int positionRequested );
-int  Stepper_GetPositionRequested( int index );
-int  Stepper_SetDuty( int index, int duty );
-int  Stepper_GetDuty( int index );
-int  Stepper_SetBipolar( int index, int bipolar );
-int  Stepper_GetBipolar( int index );
-int  Stepper_SetHalfStep( int index, int bipolar );
-int  Stepper_GetHalfStep( int index );
-int  Stepper_SetSpeed( int index, int speed );
-int  Stepper_GetSpeed( int index );
-void Stepper_SetAcceleration( int index, int acceleration );
-int  Stepper_GetAcceleration( int index );
-int Stepper_Step( int index, int steps );
+/**
+  Provides speed and position control for one or two stepper motors.
+  Up to 2 stepper motors can be controlled with the Make Application Board.
+  Specify settings for your stepper motor by setting whether it's:
+  - bipolar or unipolar
+  - normal of half-stepping
+  
+  \section Positioning
+  You can generally use the stepper motor in 2 modes - \b absolute positioning or \b relative positioning.
+
+  For absolute positioning, call Stepper_SetPositionRequested() with the desired position, and the motor will move there.
+  You can read back the stepper's position at any point along the way to determine where it is at a given moment.  The board
+  keeps an internal count of how many steps the motor has taken in order to keep track of where it is.
+
+  For relative positioning, use Stepper_Step( ) to simply move a number of steps from the current position.
+  
+  See the <a href="http://www.makingthings.com/documentation/how-to/stepper-motor">Stepper Motor how-to</a>
+  for more detailed info on hooking up a stepper motor to the Make Controller.
+  \ingroup Libraries
+*/
+class Stepper
+{
+  public:
+    Stepper(int index);
+    ~Stepper();
+
+    int position();
+    int destination();
+    int stepTo(int pos);
+    bool step(int steps);
+    bool resetPosition(int pos = 0);
+
+    int duty();
+    bool setDuty(int duty);
+
+    bool bipolar();
+    bool setBipolar(bool bipolar);
+
+    bool halfStep();
+    bool setHalfStep(bool halfstep);
+
+    int speed();
+    bool setSpeed(int speed);
+
+  protected:
+    int _index;
+    typedef struct
+    {
+      bool bipolar;
+      bool halfStep;
+      int speed;
+      int duty;
+      int destination;
+      int position;
+      Io* ios[ 4 ];
+      bool timerRunning;
+      int refcount;
+      FastTimer fastTimer;
+      Pwm* pwmA;
+      Pwm* pwmB;
+
+      void setUnipolarHalfStepOutput( int position );
+      void setUnipolarOutput( int position );
+
+      void setBipolarHalfStepOutput( int position );
+      void setBipolarOutput( int position );
+
+      void setOn( int index, int* portAOn, int* portBOn );
+      void setOff( int index, int* portAOff, int* portBOff );
+      void setAll( int portAOn, int portBOn, int portAOff, int portBOff );
+    } StepperInternal;
+    
+    friend void Stepper_IRQCallback( int id );
+    static StepperInternal* steppers[];
+
+    int getIo( int io );
+    void setDetails( );
+};
 
 /* OSC Interface */
 const char* StepperOsc_GetName( void );
