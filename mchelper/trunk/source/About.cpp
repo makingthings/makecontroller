b01cb7979aad3fdbc5a4f52277eaf3fe635e35cb
liamMT <liamMT@388e45b6-652a-11de-9f54-6d4fae1110e3>
2009-06-04 18:59:36 +0000

    - oops, left mcbuilder string in there

--- mchelper/trunk/source/About.cpp
+++ mchelper/trunk/source/About.cpp
@@ -14,10 +14,6 @@ About::About( ) : QDialog( )
   body.append(tr("By <a href=\"http://www.makingthings.com\">MakingThings</a>, %1.<br>").arg(QDate::currentDate().toString("yyyy")));
   body.append(tr("Thanks to Erik Gilling for <a href=\"http://oss.tekno.us/sam7utils\">sam7utils</a>.<br>"));
   body.append(trUtf8("French translation by Raphaël Doursenaud."));
+  resize(ui.verticalLayout->sizeHint());
 }
-
-
-
-
-
 
