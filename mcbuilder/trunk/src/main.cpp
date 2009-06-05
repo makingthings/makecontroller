6098cc12b1af40afd34855074fc38950a04a7c48
Liam Staskawicz <liam@stask.net>
2009-06-05 01:12:14 +0000

    - update dates

--- mcbuilder/trunk/src/main.cpp
+++ mcbuilder/trunk/src/main.cpp
@@ -1,6 +1,6 @@
 /*********************************************************************************
 
- Copyright 2008 MakingThings
+ Copyright 2008-2009 MakingThings
 
  Licensed under the Apache License, 
  Version 2.0 (the "License"); you may not use this file except in compliance 
