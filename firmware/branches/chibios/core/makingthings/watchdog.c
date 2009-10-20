89e79ff69bbfd266b38f047217e9a244c35a449a
Liam Staskawicz <liam@stask.net>
2009-10-20 07:58:05 +0000

    - add license header - only build this conditionally - most people will want it off, presumably

--- firmware/branches/chibios/core/makingthings/watchdog.c
+++ firmware/branches/chibios/core/makingthings/watchdog.c
@@ -1,4 +1,22 @@
+/*********************************************************************************
 
+ Copyright 2006-2009 MakingThings
+
+ Licensed under the Apache License, 
+ Version 2.0 (the "License"); you may not use this file except in compliance 
+ with the License. You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0 
+ 
+ Unless required by applicable law or agreed to in writing, software distributed
+ under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ CONDITIONS OF ANY KIND, either express or implied. See the License for
+ the specific language governing permissions and limitations under the License.
+
+*********************************************************************************/
+
+#include "config.h"
+#ifdef WATCHDOG_ENABLE
 
 #include "watchdog.h"
 #include "at91lib/AT91SAM7X256.h"
@@ -37,10 +55,13 @@ void watchdogRestart( )
 /**
   Disable the watchdog timer.
   Turn the watchdog off completely if you don't need it.
+  
+  This is done by default, unless you define WATCHDOG_ENABLE in your config.h
 */
 void watchdogDisable( )
 {
   AT91C_BASE_WDTC->WDTC_WDMR = AT91C_WDTC_WDDIS;
 }
 
+#endif // WATCHDOG_ENABLE
 
