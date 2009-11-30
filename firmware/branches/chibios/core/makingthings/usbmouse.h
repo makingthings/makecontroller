a6503b553576582c2a40e77970843264b3ae1481
lstask <lstask@388e45b6-652a-11de-9f54-6d4fae1110e3>
2009-11-30 07:03:47 +0000

    * add usb hid mouse wrapper

---
+++ firmware/branches/chibios/core/makingthings/usbmouse.h
@@ -0,0 +1,27 @@
+
+
+#ifndef USB_MOUSE_H
+#define USB_MOUSE_H
+
+#include "config.h"
+#include "types.h"
+
+typedef enum MouseClickAction_t {
+  NONE,
+  DOWN,
+  UP
+} MouseClickAction;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+void usbmouseInit(void);
+bool usbmouseIsActive(void);
+bool usbmouseSendUpdate(MouseClickAction left, MouseClickAction right, int changeX, int changeY);
+#ifdef __cplusplus
+}
+#endif
+
+#endif // USB_MOUSE_H
+
+
