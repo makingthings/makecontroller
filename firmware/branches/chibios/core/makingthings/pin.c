81a3410c5997602dbc6d4fe4c50f3e7a81b70423
Liam Staskawicz <liam@stask.net>
2009-12-06 18:28:25 +0000

    * doc updates  * make Pin a typedef enum  * change Port to Group - more consistent with pinGroup* function names

--- firmware/branches/chibios/core/makingthings/pin.c
+++ firmware/branches/chibios/core/makingthings/pin.c
@@ -16,7 +16,6 @@
 *********************************************************************************/
 
 #include "pin.h"
-#include "hal.h"
 #include "at91lib/aic.h"
 #include "core.h"
 
@@ -47,46 +46,99 @@ struct InterruptSource {
 static struct InterruptSource isrSources[MAX_INTERRUPT_SOURCES];
 static unsigned int isrSourceCount = 0;
 
-static void pinInitInterrupts(Port port, unsigned int priority);
+static void pinInitInterrupts(Group group, unsigned int priority);
 #endif // PIN_NO_ISR
 
+/** \defgroup Pins
+  Control any of the 35 pins on the Make Controller.
 
-/**
-  Read whether an Io is on or off.
-  For this to be meaningful, the Io must be configured as a \b GPIO and set as an \b input.
-  @return True if the pin is high, false if it's low.
-  
-  \b Example
+  Each of the pins on the Make Controller can be used as a digital in/out, and many
+  can be used as part of one of the peripherals on the board (serial, usb, etc).
+
+  \section Usage
+  For each pin you'll generally want to first set its mode, then control it.  The most
+  common modes are INPUT and OUTPUT - here's how they work.
+
+  \subsection Input
+  First set a pin as an input, then you can read the value on that pin:
   \code
-  Io io(IO_PB28, Io::GPIO, false); // new io, configured as a GPIO and an input
-  bool is_pb28_on = io.value(); // is it on?
+  pinSetMode(PIN_PA08, INPUT);
+  bool isItOn = pinValue(PIN_PA08);
   \endcode
+
+  \subsection Output
+  First set a pin as an output, then you can turn it on and off:
+  \code
+  pinSetMode(PIN_PA08, OUTPUT);
+  pinSetValue(ON);  // turn it on
+  pinSetValue(OFF); // turn it off
+  \endcode
+
+  \section Interrupts
+  You can also register to get notified immediately when the value on a pin
+  has changed, rather than constantly reading the value to see if it has changed.
+  This can be much more efficient if you need to know precisely when a pin's value
+  has changed - see pinAddInterruptHandler() for details.
+
+  \section group Group Functions
+  For most pin functions, there are corresponding \b group functions that allow you
+  to perform the same action simultaneously on many pins.  To specify a group of pins,
+  use the symbols from \ref PinBits as follows:
+  \code int pingroup = (PIN_PA13_BIT | PIN_PA14_BIT | PIN_PA15_BIT); \endcode
+  to turn these all off at the same time, it's as easy as:
+  \code pinGroupOff(GROUP_A, pingroup); \endcode
+  Note that within a group you can only specify pins from the same port - \b GROUP_A or \b GROUP_B.
+  @{
 */
 
-bool pinValue(int pin)
+/**
+ * Read whether a pin is on or off.
+ * The pin can be configured as an \b INPUT or \b OUTPUT.
+ * @param pin Which pin to read.
+ * @return True if the pin is high, false if it's low.
+ * \b Example
+ * \code
+ * pinSetMode(PIN_PB28, INPUT); // set as an input
+ * if (pinValue(PIN_PB28) == ON) {
+ *   // then it's on
+ * }
+ * else {
+ *   // then it's off
+ * }
+ * \endcode
+ */
+bool pinValue(Pin pin)
 {
   return palReadPad(IOPORT(pin), PIN(pin));
 }
 
-int pinGroupValue(Port port)
+/**
+ * Same as pinValue() but for a group of pins at once.
+ * @param group Which group of pins to read - GROUP_A or GROUP_B
+ * @return The value for that group of pins.
+ */
+int pinGroupValue(Group group)
 {
-  return palReadPort(port);
+  return palReadPort(group);
 }
 
 /**
-  Turn an Io on or off.
-  For this to be meaningful, the Io must be configured as a \b GPIO and set as an \b output.
-  If you want to turn the Io on or off directly, use on() or off().
-  @param onoff True to turn it on, false to turn it off.
+  Turn a pin on or off.
+  To be meaningful, the pin must be configured as an \b OUTPUT.
+  If you want to turn the pin on or off directly, use pinOn() or pinOff().
+
+  You can use the ON and OFF symbols to specify the value.
+  @param pin Which pin to control.
+  @param value True to turn it on, false to turn it off.
   @return True on success, false on failure.
   
   \b Example
   \code
-  Io io(IO_PB28); // new io, defaults to GPIO output
-  io.setValue(true); // turn it on
+  pinSetMode(PIN_PB28, OUTPUT); // set it as an output
+  pinSetValue(ON);              // turn it on
   \endcode
 */
-void pinSetValue(int pin, bool value)
+void pinSetValue(Pin pin, bool value)
 {
   if (value) {
     palSetPad(IOPORT(pin), PIN(pin));
@@ -96,108 +148,132 @@ void pinSetValue(int pin, bool value)
   }
 }
 
-void pinGroupSetValue(Port port, int pins, bool value)
+/**
+ * Same as pinSetValue() but for a group of pins.
+ * @param group Which port the pins are on.
+ * @param pins Which pins to control.
+ * @param value Which value to set the pins to.
+ */
+void pinGroupSetValue(Group group, int pins, bool value)
 {
   if(value) {
-    palSetPort(port, pins);
+    palSetPort(group, pins);
   }
   else {
-    palClearPort(port, pins);
+    palClearPort(group, pins);
   }
 }
 
 /**
-  Turn on Io on.
-  For this to be meaningful, the Io must be configured as a \b GPIO and set as an \b output.
+  Turn a pin on.
+  To be meaningful, the pin must be configured as an \b OUTPUT.
   
-  This is slightly faster than setValue() since it doesn't have to check
+  This is slightly faster than pinSetValue() since it doesn't have to check
   whether to turn it on or off.
+  @param pin Which pin to turn on.
   @return True on success, false on failure.
   
   \b Example
   \code
-  Io io(IO_PB18); // defaults to GPIO output
-  io.on(); // turn it on
+  pinSetMode(PIN_PB18, OUTPUT); // turn it into an output
+  pinOn(PIN_PB18);              // turn it on
   \endcode
 */
-void pinOn(int pin)
+void pinOn(Pin pin)
 {
   palSetPad(IOPORT(pin), PIN(pin));
 }
 
-void pinGroupOn(Port port, int pins)
+/**
+ * Same as pinOn() but for a group of pins.
+ * @param group Which port are the pins on.
+ * @param pins The pins to turn on
+ */
+void pinGroupOn(Group group, int pins)
 {
-  palSetPort(port, pins);
+  palSetPort(group, pins);
 }
 
 /**
-  Turn an Io off.
-  For this to be meaningful, the Io must be configured as a \b GPIO and set as an \b output.
+  Turn a pin off.
+  To be meaningful, this pin must be configured as an \b OUTPUT.
   
-  This is slightly faster than setValue() since it doesn't have to check
+  This is slightly faster than pinSetValue() since it doesn't have to check
   whether to turn it on or off.
+  @param pin Which pin to turn off.
   @return True on success, false on failure.
   
   \b Example
   \code
-  Io io(IO_PB18); // defaults to GPIO output
-  io.off(); // turn it off
+  pinSetMode(PIN_PB18, OUTPUT); // turn it into an output
+  pinOff(PIN_PB18);             // turn it off
   \endcode
 */
-void pinOff(int pin)
+void pinOff(Pin pin)
 {
   palClearPad(IOPORT(pin), PIN(pin));
 }
 
-void pinGroupOff(Port port, int pins)
+/**
+ * Same as pinOff() but for a group of pins.
+ * @param group Which port are the pins on.
+ * @param pins The pins to turn off
+ */
+void pinGroupOff(Group group, int pins)
 {
-  palClearPort(port, pins);
+  palClearPort(group, pins);
 }
 
 /**
-  Set an Io as an input or an output
-  For this to be meaningful, the Io must be configured as a \b GPIO.
-  
-  You can also use the \b OUTPUT and \b INPUT symbols instead of true/false, 
-  for convenience.
-  @param output True to set it as an output, false to set it as an input.
+  Set the mode for a pin.
+  Pins can operate in a variety of modes - see PinMode for options.
+  @param pin Which pin to set the mode for.
+  @param mode Which PinMode to use for this pin.
   
   \b Example
   \code
-  Io io(IO_PA01); // is a GPIO output by default
-  io.setDirection(INPUT); // now change it to an input
+  pinSetMode(PIN_PA1, INPUT);
   \endcode
 */
-void pinSetMode(int pin, PinMode mode)
+void pinSetMode(Pin pin, PinMode mode)
 {
   pinGroupSetMode(IOPORT(pin), PIN_MASK(pin), mode);
 }
 
-void pinGroupSetMode(Port port, int pins, PinMode mode)
+/**
+ * Same as pinSetMode(), but for a group of pins.
+ * \b Example
+ * \code
+ * // set pins PA0, PA1, PA2 as outputs, all at once
+ * int pingroup = (PIN_PA0_BIT | PIN_PA1_BIT | PIN_PA2_BIT); // the group of pins to control
+ * pinGroupSetMode(GROUP_A, pingroup, OUTPUT);
+ * \endcode
+ */
+void pinGroupSetMode(Group group, int pins, PinMode mode)
 {
   switch(mode) {
     case PERIPHERAL_A:
-      port->PIO_PDR = pins;
-      port->PIO_ASR = pins;
+      group->PIO_PDR = pins;
+      group->PIO_ASR = pins;
       break;
     case PERIPHERAL_B:
-      port->PIO_PDR = pins;
-      port->PIO_BSR = pins;
+      group->PIO_PDR = pins;
+      group->PIO_BSR = pins;
       break;
     case PULLUP_ON:
-      port->PIO_PPUER = pins;
+      group->PIO_PPUER = pins;
       break;
     case PULLUP_OFF:
-      port->PIO_PPUDR = pins;
+      group->PIO_PPUDR = pins;
       break;
     case GLITCH_FILTER_ON:
-      port->PIO_IFER = pins;
+      group->PIO_IFER = pins;
       break;
     case GLITCH_FILTER_OFF:
-      port->PIO_IFDR = pins;
+      group->PIO_IFDR = pins;
       break;
     default:
-      palSetGroupMode(port, pins, mode);
+      palSetGroupMode(group, pins, mode);
       break;
   }
 }
@@ -205,48 +281,51 @@ void pinGroupSetMode(Port port, int pins, PinMode mode)
 #ifndef PIN_NO_ISR
 
 /**
-  Add an interrupt handler for this signal.
-  If you want to get notified when the signal on this pin changes,
-  you can register an interrupt handler, instead of constantly reading the pin.
-  To do this, define a function you want to be called when the pin changes, and
-  then register it with the Io you want to monitor.  The function has to have a
-  specific signature - it must be of the form:
+  Add an interrupt handler for this pin.
+  Your handler will get called any time the value on this pin changes,
+  meaning you don't have to constantly check its value yourself.
+  Your handler must have this specific signature:
   \code void myHandler(void* context) \endcode
   
-  This function will be called any time the signal on the Io pin changes.  When you 
-  register the handler, you can provide a pointer to some context that will be passed
-  into your handler.  This can be an instance of a class, if you want to 
+  Note that in your handler, you shouldn't do anything that takes too long,
+  and definitely can't sleep().
+
+  You must first set the mode the of the pin to \b INPUT.  Since the handler
+  is called whenever there's a change, you don't necessarily know if it turned
+  on or off, but you can check from within the handler:
   
   \b Example
   \code
-  void myHandler(void* context); // declare our handler
-  
+  int count = 0; // we'll keep track of how many changes we've seen
+  void myHandler(void* context)
+  {
+    if(pinValue(PIN_PB27) == ON) {
+      // then the change was from low to high
+    }
+    else {
+      // it was from high to low
+    }
+
+    count++; // increment our count
+  }
+
   void myTask(void* p)
   {
-    Io io(IO_PB27, Io::GPIO, INPUT);   // Io on AnalogIn 0 - as digital input
-    io.addInterruptHandler(myHandler); // register our handler
+    pinSetMode(PIN_PB27, INPUT);       // set the pin as an input
+    pinAddInterruptHandler(PIN_PB27, myHandler, 0); // register our handler
     
-    while(true)
-    {
+    while(true) {
       // do the rest of my task...
     }
   }
-  
-  int count = 0; // how many times has our interrupt triggered?
-  // now this will get called every time the value on PB27 changes
-  void myHandler(void* context)
-  {
-    count++;
-    if(count > 100)
-      count = 0;
-  }
   \endcode
   
-  @param h The function to be called when there's an interrupt
+  @param pin The pin to monitor for changes.
+  @param h Your handler that will be called when the input value changes.
   @param arg (optional) Argument that will be passed into your handler, if desired.
   @return True if the handler was registered successfully, false if not.
 */
-bool pinAddInterruptHandler(int pin, PinInterruptHandler h, void* arg)
+bool pinAddInterruptHandler(Pin pin, PinInterruptHandler h, void* arg)
 {
   if(isrSourceCount >= MAX_INTERRUPT_SOURCES)
     return false;
@@ -258,7 +337,7 @@ bool pinAddInterruptHandler(int pin, PinInterruptHandler h, void* arg)
     pinInitInterrupts(IOPORT(pin), (AT91C_AIC_SRCTYPE_INT_HIGH_LEVEL | 3) );
   
   IOPORT(pin)->PIO_ISR;           // clear the status register
-  pinEnableInterruptHandler(pin); // enable our channel
+  pinEnableHandler(pin); // enable our channel
 
   isrSources[isrSourceCount].callback = h;
   isrSources[isrSourceCount++].context = arg; // make sure to increment our handler count
@@ -267,35 +346,53 @@ bool pinAddInterruptHandler(int pin, PinInterruptHandler h, void* arg)
 }
 
 /**
-  Disable the interrupt handler for this io line.
-  If you've already registered an interrupt with addInterruptHandler()
-  this will disable it.
+  Disable the interrupt handler for this pin.
+  This is only meaningful if you have registered a handler with
+  pinAddInterruptHandler().
+  @param pin The pin to disable the handler for.
   
   \b Example
   \code
-  Io io(IO_PB27, Io::GPIO, INPUT);
-  io.addInterruptHandler(myHandler); // start notifications
+  pinSetMode(PIN_PB27, INPUT);
+  pinAddInterruptHandler(PIN_PB27, myHandler); // start notifications
   
   // ... some time later, we want to stop getting notified
   
-  io.removeInterruptHandler( ); // stop notifications
+  pinDisableHandler(PIN_PB27); // stop notifications
   \endcode
-  
-  @return True on success, false on failure.
 */
-void pinDisableInterruptHandler(int pin)
+void pinDisableHandler(Pin pin)
 {
   IOPORT(pin)->PIO_IDR = PIN_MASK(pin);
 }
 
-void pinEnableInterruptHandler(int pin)
+/**
+ * Re-enable the interrupt handler for a pin.
+ * This is to re-enable a handler that has previously been
+ * disabled via pinDisableHandler().
+ * @param pin The pin to re-enable the handler for.
+ *
+ * \b Example
+ * \code
+ * pinSetMode(PIN_PB27, INPUT);
+ * pinAddInterruptHandler(PIN_PB27, myHandler); // start notifications
+ * // ... some time later, we want to stop getting notified
+ * pinDisableHandler(PIN_PB27); // stop notifications
+ * // ... some time later yet again, we want to turn them back on
+ * pinEnableHandler(PIN_PB27);
+ * \endcode
+ */
+void pinEnableHandler(Pin pin)
 {
   IOPORT(pin)->PIO_IER = PIN_MASK(pin);
 }
 
-static void pinServeInterrupt( Port port )
+/** @}
+*/
+
+static void pinServeInterrupt( Group group )
 {
-  unsigned int status = port->PIO_ISR & port->PIO_IMR;
+  unsigned int status = group->PIO_ISR & group->PIO_IMR;
 
   // Check pending events
   if(status) {
@@ -305,7 +402,7 @@ static void pinServeInterrupt( Port port )
     for( i = 0; status != 0  && i < isrSourceCount; i++ ) {
       is = &(isrSources[i]);
       pinMask = PIN_MASK(is->pin);
-      if( (IOPORT(is->pin) == port) && ((status & pinMask) != 0) ) {
+      if( (IOPORT(is->pin) == group) && ((status & pinMask) != 0) ) {
         is->callback(is->context);     // callback the handler
         status &= ~(pinMask);          // mark this channel as serviced
       }
@@ -333,17 +430,17 @@ static CH_IRQ_HANDLER( pinIsrB ) {
   Turn on interrupts for a pio channel - a or b
   at a given priority.
 */
-void pinInitInterrupts(Port port, unsigned int priority)
+void pinInitInterrupts(Group group, unsigned int priority)
 {
   unsigned int chan;
   void (*isr_handler)(void);
   
-  if( port == AT91C_BASE_PIOA ) {
+  if( group == AT91C_BASE_PIOA ) {
     chan = AT91C_ID_PIOA;
     isr_handler = pinIsrA;
   }
 #if SAM7_PLATFORM == SAM7X128 || SAM7_PLATFORM == SAM7X256 || SAM7_PLATFORM == SAM7X512
-  else if( port == AT91C_BASE_PIOB ) {
+  else if( group == AT91C_BASE_PIOB ) {
     chan = AT91C_ID_PIOB;
     isr_handler = pinIsrB;
   }
@@ -351,8 +448,8 @@ void pinInitInterrupts(Port port, unsigned int priority)
   else
     return;
   
-  port->PIO_ISR;                                // clear with a read
-  port->PIO_IDR = 0xFFFFFFFF;                   // disable all by default
+  group->PIO_ISR;                                // clear with a read
+  group->PIO_IDR = 0xFFFFFFFF;                   // disable all by default
   AIC_ConfigureIT(chan, priority, isr_handler); // set it up
   AIC_EnableIT(chan);
 }
--- firmware/branches/chibios/core/makingthings/pin.h
+++ firmware/branches/chibios/core/makingthings/pin.h
@@ -25,58 +25,20 @@
 #include "hal.h"
 
 /**
-  Control any of the 35 Input/Output signals on the Make Controller.
-  
-  Each of the signals on the Make Controller can be used as a digital in/out, and many
-  can be used as part of one of the peripherals on the board (serial, usb, etc).  The Io 
-  class allows you to easily control these signals.
-  
-  \section Usage
-  The most common way to use the Io system is to control pins as GPIO (general purpose in/out).
-  To do this, create a new Io object, specifying that you want it to be a GPIO and whether it should
-  be an input or an output.  Check the list of signal names at \ref IoIndices.
-  
-  \code
-  Io mysensor(PIN_PA08, Io::GPIO, INPUT); // a new digital in on PA08, as a GPIO input
-  bool isOn = mysensor.value(); // now read the input with the value() method
-  \endcode
-  
-  You can just as easily set it up as a digital out, and turn it on and off:
-  \code
-  Io myLED(PIN_PA08, Io::GPIO, OUTPUT); // a new digital in on PA08, as a GPIO output
-  myLED.on(); // turn it on
-  myLED.off(); // turn it off
-  // or do it with setValue if you want to keep the state in a variable
-  bool state = true;
-  myLED.setValue(state); // turn it on
-  state = false;
-  myLED.setValue(state); // turn it off
-  
-  // we can even change it back into an input
-  myLED.setDirection(INPUT);
-  // hmm...the myLED name doesn't make so much sense now...oh well
-  \endcode
-  
-  \section Interrupts
-  You can also register to get notified automatically, when the status on an Io
-  line changes, rather than constantly reading the value to see if it has changed.  
-  This can be much more efficient - see addInterruptHandler() and removeInterruptHandler() 
-  for details.
-  
-  \section settings Additional Settings
-  Each Io pin also has some additional settings that can optionally be configured.
-  - \b Pullup - each signal has an optional pullup resistor that can be enabled.  To 
-  configure this, use the pullup() and setPullup() methods.
-  - <b>Glitch Filter</b> - each signal also has a glitch filter that can help reject 
-  short signals (most signals less than a master clock cycle, which is roughly 20 ns).
-  Check the filter() and setFilter() methods.
-  
-  \ingroup io
-*/
+ * @brief Types for pins.
+ * @addtogroup Pins
+ * @{
+ */
 
+/**
+ * A handler for use with pinAddInterruptHandler().
+ */
 typedef void (*PinInterruptHandler)(void*);
 
-typedef enum PinMode_t {
+/**
+ * Available pin modes.
+ */
+typedef enum {
   INPUT = PAL_MODE_INPUT,
   INPUT_PULLUP = PAL_MODE_INPUT_PULLUP,
   OUTPUT = PAL_MODE_OUTPUT_PUSHPULL,
@@ -89,117 +51,108 @@ typedef enum PinMode_t {
   GLITCH_FILTER_OFF
 } PinMode;
 
-typedef AT91S_PIO* Port;
-#define PORT_A IOPORT1
-#define PORT_B IOPORT2
-
-bool pinValue(int pin);
-int  pinGroupValue(Port port);
-void pinSetValue(int pin, bool value);
-void pinGroupSetValue(Port port, int pins, bool value);
-void pinOn(int pin);
-void pinGroupOn(Port port, int pins);
-void pinOff(int pin);
-void pinGroupOff(Port port, int pins);
-void pinSetMode(int pin, PinMode mode);
-void pinGroupSetMode(Port port, int pins, PinMode mode);
-#ifndef PIN_NO_ISR
-bool pinAddInterruptHandler(int pin, PinInterruptHandler h, void* arg);
-void pinDisableInterruptHandler(int pin);
-void pinEnableInterruptHandler(int pin);
-#endif // PIN_NO_ISR
+typedef AT91S_PIO* Group;
+#define GROUP_A IOPORT1
+#define GROUP_B IOPORT2
 
 /**
-  \defgroup IoIndices IO Indices
-  Indices (0-63) for each of the processor's IO lines.  PA0-PA31 are represented
-  by indices 0 - 31, PB0-PB31 by indices 32 - 63.
-  \ingroup Io
-  @{
-*/
+ * Pins for each of the processor's IO lines.
+ */
+typedef enum {
+  PIN_PA0  = 0,  /**< Pin 0, Port A */
+  PIN_PA1  = 1,  /**< Pin 1, Port A */
+  PIN_PA2  = 2,  /**< Pin 2, Port A */
+  PIN_PA3  = 3,  /**< Pin 3, Port A */
+  PIN_PA4  = 4,  /**< Pin 4, Port A */
+  PIN_PA5  = 5,  /**< Pin 5, Port A */
+  PIN_PA6  = 6,  /**< Pin 6, Port A */
+  PIN_PA7  = 7,  /**< Pin 7, Port A */
+  PIN_PA8  = 8,  /**< Pin 8, Port A */
+  PIN_PA9  = 9,  /**< Pin 9, Port A */
+  PIN_PA10 = 10, /**< Pin 10, Port A */
+  PIN_PA11 = 11, /**< Pin 11, Port A */
+  PIN_PA12 = 12, /**< Pin 12, Port A */
+  PIN_PA13 = 13, /**< Pin 13, Port A */
+  PIN_PA14 = 14, /**< Pin 14, Port A */
+  PIN_PA15 = 15, /**< Pin 15, Port A */
+  PIN_PA16 = 16, /**< Pin 16, Port A */
+  PIN_PA17 = 17, /**< Pin 17, Port A */
+  PIN_PA18 = 18, /**< Pin 18, Port A */
+  PIN_PA19 = 19, /**< Pin 19, Port A */
+  PIN_PA20 = 20, /**< Pin 20, Port A */
+  PIN_PA21 = 21, /**< Pin 21, Port A */
+  PIN_PA22 = 22, /**< Pin 22, Port A */
+  PIN_PA23 = 23, /**< Pin 23, Port A */
+  PIN_PA24 = 24, /**< Pin 24, Port A */
+  PIN_PA25 = 25, /**< Pin 25, Port A */
+  PIN_PA26 = 26, /**< Pin 26, Port A */
+  PIN_PA27 = 27, /**< Pin 27, Port A */
+  PIN_PA28 = 28, /**< Pin 28, Port A */
+  PIN_PA29 = 29, /**< Pin 29, Port A */
+  PIN_PA30 = 30, /**< Pin 30, Port A */
+  PIN_PA31 = 31, /**< Pin 31, Port A */
 
-#define PIN_PA0  0  /**< Pin 0, Port A */      
-#define PIN_PA1  1  /**< Pin 1, Port A */      
-#define PIN_PA2  2  /**< Pin 2, Port A */      
-#define PIN_PA3  3  /**< Pin 3, Port A */      
-#define PIN_PA4  4  /**< Pin 4, Port A */      
-#define PIN_PA5  5  /**< Pin 5, Port A */      
-#define PIN_PA6  6  /**< Pin 6, Port A */      
-#define PIN_PA7  7  /**< Pin 7, Port A */      
-#define PIN_PA8  8  /**< Pin 8, Port A */      
-#define PIN_PA9  9  /**< Pin 9, Port A */      
-#define PIN_PA10 10 /**< Pin 10, Port A */
-#define PIN_PA11 11 /**< Pin 11, Port A */
-#define PIN_PA12 12 /**< Pin 12, Port A */
-#define PIN_PA13 13 /**< Pin 13, Port A */
-#define PIN_PA14 14 /**< Pin 14, Port A */
-#define PIN_PA15 15 /**< Pin 15, Port A */
-#define PIN_PA16 16 /**< Pin 16, Port A */
-#define PIN_PA17 17 /**< Pin 17, Port A */
-#define PIN_PA18 18 /**< Pin 18, Port A */
-#define PIN_PA19 19 /**< Pin 19, Port A */
-#define PIN_PA20 20 /**< Pin 20, Port A */
-#define PIN_PA21 21 /**< Pin 21, Port A */
-#define PIN_PA22 22 /**< Pin 22, Port A */
-#define PIN_PA23 23 /**< Pin 23, Port A */
-#define PIN_PA24 24 /**< Pin 24, Port A */
-#define PIN_PA25 25 /**< Pin 25, Port A */
-#define PIN_PA26 26 /**< Pin 26, Port A */
-#define PIN_PA27 27 /**< Pin 27, Port A */
-#define PIN_PA28 28 /**< Pin 28, Port A */
-#define PIN_PA29 29 /**< Pin 29, Port A */
-#define PIN_PA30 30 /**< Pin 30, Port A */
-#define PIN_PA31 31 /**< Pin 31, Port A */
-
-#define PIN_PB00 32 /**< Pin 0, Port B */
-#define PIN_PB01 33 /**< Pin 1, Port B */
-#define PIN_PB02 34 /**< Pin 2, Port B */
-#define PIN_PB03 35 /**< Pin 3, Port B */
-#define PIN_PB04 36 /**< Pin 4, Port B */
-#define PIN_PB05 37 /**< Pin 5, Port B */
-#define PIN_PB06 38 /**< Pin 6, Port B */
-#define PIN_PB07 39 /**< Pin 7, Port B */
-#define PIN_PB08 40 /**< Pin 8, Port B */
-#define PIN_PB09 41 /**< Pin 9, Port B */
-#define PIN_PB10 42 /**< Pin 10, Port B */
-#define PIN_PB11 43 /**< Pin 11, Port B */
-#define PIN_PB12 44 /**< Pin 12, Port B */
-#define PIN_PB13 45 /**< Pin 13, Port B */
-#define PIN_PB14 46 /**< Pin 14, Port B */
-#define PIN_PB15 47 /**< Pin 15, Port B */
-#define PIN_PB16 48 /**< Pin 16, Port B */
-#define PIN_PB17 49 /**< Pin 17, Port B */
-#define PIN_PB18 50 /**< Pin 18, Port B */
-#define PIN_PB19 51 /**< Pin 19, Port B */
-#define PIN_PB20 52 /**< Pin 20, Port B */
-#define PIN_PB21 53 /**< Pin 21, Port B */
-#define PIN_PB22 54 /**< Pin 22, Port B */
-#define PIN_PB23 55 /**< Pin 23, Port B */
-#define PIN_PB24 56 /**< Pin 24, Port B */
-#define PIN_PB25 57 /**< Pin 25, Port B */
-#define PIN_PB26 58 /**< Pin 26, Port B */
-#define PIN_PB27 59 /**< Pin 27, Port B */
-#define PIN_PB28 60 /**< Pin 28, Port B */
-#define PIN_PB29 61 /**< Pin 29, Port B */
-#define PIN_PB30 62 /**< Pin 30, Port B */
-#define PIN_PB31 63 /**< Pin 31, Port B */
+  PIN_PB00 = 32, /**< Pin 0, Port B */
+  PIN_PB01 = 33, /**< Pin 1, Port B */
+  PIN_PB02 = 34, /**< Pin 2, Port B */
+  PIN_PB03 = 35, /**< Pin 3, Port B */
+  PIN_PB04 = 36, /**< Pin 4, Port B */
+  PIN_PB05 = 37, /**< Pin 5, Port B */
+  PIN_PB06 = 38, /**< Pin 6, Port B */
+  PIN_PB07 = 39, /**< Pin 7, Port B */
+  PIN_PB08 = 40, /**< Pin 8, Port B */
+  PIN_PB09 = 41, /**< Pin 9, Port B */
+  PIN_PB10 = 42, /**< Pin 10, Port B */
+  PIN_PB11 = 43, /**< Pin 11, Port B */
+  PIN_PB12 = 44, /**< Pin 12, Port B */
+  PIN_PB13 = 45, /**< Pin 13, Port B */
+  PIN_PB14 = 46, /**< Pin 14, Port B */
+  PIN_PB15 = 47, /**< Pin 15, Port B */
+  PIN_PB16 = 48, /**< Pin 16, Port B */
+  PIN_PB17 = 49, /**< Pin 17, Port B */
+  PIN_PB18 = 50, /**< Pin 18, Port B */
+  PIN_PB19 = 51, /**< Pin 19, Port B */
+  PIN_PB20 = 52, /**< Pin 20, Port B */
+  PIN_PB21 = 53, /**< Pin 21, Port B */
+  PIN_PB22 = 54, /**< Pin 22, Port B */
+  PIN_PB23 = 55, /**< Pin 23, Port B */
+  PIN_PB24 = 56, /**< Pin 24, Port B */
+  PIN_PB25 = 57, /**< Pin 25, Port B */
+  PIN_PB26 = 58, /**< Pin 26, Port B */
+  PIN_PB27 = 59, /**< Pin 27, Port B */
+  PIN_PB28 = 60, /**< Pin 28, Port B */
+  PIN_PB29 = 61, /**< Pin 29, Port B */
+  PIN_PB30 = 62, /**< Pin 30, Port B */
+  PIN_PB31 = 63, /**< Pin 31, Port B */
+} Pin;
 
 /* @} */
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+bool pinValue(Pin pin);
+int  pinGroupValue(Group port);
+void pinSetValue(Pin pin, bool value);
+void pinGroupSetValue(Group port, int pins, bool value);
+void pinOn(Pin pin);
+void pinGroupOn(Group port, int pins);
+void pinOff(Pin pin);
+void pinGroupOff(Group port, int pins);
+void pinSetMode(Pin pin, PinMode mode);
+void pinGroupSetMode(Group port, int pins, PinMode mode);
+#ifndef PIN_NO_ISR
+bool pinAddInterruptHandler(Pin pin, PinInterruptHandler h, void* arg);
+void pinDisableHandler(Pin pin);
+void pinEnableHandler(Pin pin);
+#endif // PIN_NO_ISR
+#ifdef __cplusplus
+}
+#endif
+
 /**
-\defgroup IoBits IO Bits
-  The values to use to create a mask to pass into any of the \b Bits style functions.  
-  Your mask values need to be of type longlong since it needs to represent 64 bits, 
-  for the 64 IO lines.
-
-  \b Example
-
-  \code
-  longlong mymask = 0;
-  mymask |= (PIN_PA00_BIT | PIN_PA03_BIT | PIN_PA11_BIT);
-  // now disable the pullups for lines PA00, PA03, and PA11
-  PIN_PullupDisableBits( mymask );
-  \endcode
-\ingroup Io
+  \defgroup PinBits Pin Bits
+  @addtogroup Pins
 @{
 */
 
