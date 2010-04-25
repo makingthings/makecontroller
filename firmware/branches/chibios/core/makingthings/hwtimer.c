f270174022956792c9faf056992c0fb9660e6f0d
lstask <lstask@388e45b6-652a-11de-9f54-6d4fae1110e3>
2010-04-25 00:26:12 +0000

    * renaming timer to hwtimer

--- firmware/branches/chibios/core/makingthings/hwtimer.c
+++ firmware/branches/chibios/core/makingthings/hwtimer.c
@@ -16,7 +16,7 @@
 *********************************************************************************/
 
 #include "types.h"
-#include "timer.h"
+#include "hwtimer.h"
 #include "error.h"
 #include "at91lib/aic.h"
 #include <ch.h>
