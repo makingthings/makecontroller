6e8a06fe02e2fce6f15991b00b4d4e7b5fff4ad9
Liam Staskawicz <liam@stask.net>
2010-04-30 05:33:59 +0000

    * new appled OSC code

--- firmware/branches/chibios/libraries/appled/appled.c
+++ firmware/branches/chibios/libraries/appled/appled.c
@@ -118,117 +118,65 @@ bool appledValue(int led)
 
 #ifdef OSC
 
+#include "osc.h"
+
 /**
-  \defgroup AppLEDOSC App LED - OSC
+  \defgroup AppLEDOSC OSC - App LED
   Control the Application Board's Status LEDs via OSC.
-	
-	\section devices Devices
-	There are 4 LEDs on the Make Application Board, numbered 0 - 3.
-	
-	\section properties Properties
-	The LEDs have the following properties:
+
+  \section devices Devices
+  There are 4 LEDs on the Make Application Board, numbered 0 - 3.
+
+  \section properties Properties
+  The LEDs have the following properties:
   - state
   - active
 
-	\par State
-	The \b state property corresponds to the on/off state of a given LED.
-	For example, to turn on the first LED, send a message like
-	\code /appled/0/state 1 \endcode
-	To turn it off, send the message \code /appled/0/state 0 \endcode
-	
-	\par Active
-	The \b active property corresponds to the active state of an LED.
-	If an LED is set to be active, no other tasks will be able to
-	write to it.  If you're not seeing appropriate
-	responses to your messages to the LED, check the whether it's 
-	locked by sending a message like
-	\code /appled/0/active \endcode
-	\par
-	You can set the active flag by sending
-	\code /appled/0/active 1 \endcode
+  \par State
+  The \b state property corresponds to the on/off state of a given LED.
+  For example, to turn on the first LED, send a message like
+  \code /appled/0/state 1 \endcode
+  To turn it off, send the message \code /appled/0/state 0 \endcode
+
+  \par Active
+  The \b active property corresponds to the active state of an LED.
+  If an LED is set to be active, no other tasks will be able to
+  write to it.  If you're not seeing appropriate
+  responses to your messages to the LED, check the whether it's
+  locked by sending a message like
+  \code /appled/0/active \endcode
+  \par
+  You can set the active flag by sending
+  \code /appled/0/active 1 \endcode
 */
 
-#include "osc.h"
-#include "string.h"
-#include "stdio.h"
-
-const char* AppLedOSC::propertyList[] = {"state", 0};
-
-/*
-  We expect to get a message like /appled/index/property (data)
-*/
-int AppLedOSC::onNewMsg( OscTransport t, OscMessage* msg, int src_addr, int src_port )
+static bool appledOscHandler(OscChannel ch, char* address, short idx, OscData d[], int datalen)
 {
-  (void)src_addr;
-  (void)src_port;
-  int replies = 0;
-  char* property = msg->addressElementAsString( 2 ); // address element 2 should be the property
-  if( !property )
-    return replies;
-  
-  int prop_index = propertyLookup( propertyList, property );
-  if( prop_index < 0 ) // not one of our properties
-    return replies;
-  
-  int index;
-  OscRangeHelper rangeHelper(msg, 1, APPLED_COUNT); // element 1 should be our index
-  while( rangeHelper.hasNext() )
-  {
-    index = rangeHelper.next();
-    if( index < 0 || index >= APPLED_COUNT )
-      continue;
-    AppLed led(index);
-    if( msg->data_count ) // setter...write the value to the led
-    {
-      switch( prop_index )
-      {
-        case 0: // state
-          led.setState(msg->dataItemAsInt(0));
-          break;
-      }
-    }
-    else // getter...return a message
-    {
-      switch( prop_index )
-      {
-        case 0: // state
-          Osc::get()->createMessage(t, msg->address, ",i", led.getState());
-          replies++;
-          break;
-      }
-    }
+  if (datalen == 1) {
+    appledSetValue(idx, d[0].value.i);
+    return true;
   }
-  return replies;
+  else if (datalen == 0) {
+    OscData d;
+    d.value.i = appledValue(idx);
+    oscCreateMessage(ch, address, &d, 1);
+    return true;
+  }
+  return false;
 }
 
-int AppLedOSC::onQuery( OscTransport t, char* address, int element )
-{
-  int replies = 0;
-  switch( element )
-  {
-    case 1: // index
-    {
-      int i;
-      for(i = 0; i < APPLED_COUNT; i++)
-      {
-        Osc::get()->createMessage(t, address, ",i", i);
-        replies++;
-      }
-      break;
-    }
-    case 2: // property
-    {
-      const char** prop = propertyList; // create message for each item in our propertyList
-      while(prop)
-      {
-        Osc::get()->createMessage(t, address, ",s", *prop);
-        replies++;
-        prop++;
-      }
-      break;
-    }
-  }
-  return replies;
-}
+static const OscNode appledState = {
+  .name = "state",
+  .handler = appledOscHandler
+};
+static const OscNode appledRange = {
+  .range = 4,
+  .children = { &appledState, 0 }
+};
+const OscNode appledOsc = {
+  .name = "appled",
+  .children = { &appledRange, 0 }
+};
 
 #endif // OSC
+
