004bc64e9f3e1f9ee9d530c939273e17ca78560f
lstask <lstask@388e45b6-652a-11de-9f54-6d4fae1110e3>
2010-05-25 05:03:33 +0000

    * woops, didn't mean to commit some of those

--- firmware/trunk/projects/heavy/heavy.c
+++ firmware/trunk/projects/heavy/heavy.c
@@ -1,36 +1,9 @@
 
 #include "core.h"
-#include "appled.h"
-
-//const OscNode oscSystems = {
-//
-//};
-
-#define SZ 256
-char buf[SZ];
-
-Timer t;
-int tester;
-
-void timerHandler(void)
-{
-  tester++;
-  timerGo(&t, 500, timerHandler);
-}
 
 void setup()
 {
-//  int i;
-  // setup all the systems we want to use
-  appledEnable();
-//  digitaloutInit();
-//  dipswitchInit();
-
-  usbserialInit();
-  tester = 0;
-  timerGo(&t, 500, timerHandler);
-//  networkInit();
-//  char c = serialGet(0, 0);
+  // nothing to do here, for now
 }
 
 void loop()
@@ -39,10 +12,5 @@ void loop()
   sleep(10);
   ledSetValue(OFF);
   sleep(990);
-  int rxed = usbserialRead(buf, sizeof(buf), -1);
-  if (rxed > 0) {
-    usbserialWrite(buf, rxed, 1000);
-  }
-//  usbserialWrite("hi\n", 3, 1000);
 }
 
