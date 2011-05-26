e0ab832ed3c859b4f610defb3af031259deb51cb
lstask <lstask@388e45b6-652a-11de-9f54-6d4fae1110e3>
2011-05-26 03:23:29 +0000

    * transition xml server to using QXmlStreamReader

--- mchelper/trunk/include/OscXmlServer.h
+++ mchelper/trunk/include/OscXmlServer.h
@@ -20,9 +20,8 @@
 
 #include <QTcpServer>
 #include <QTcpSocket>
-#include <QXmlSimpleReader>
-#include <QXmlDefaultHandler>
 #include <QXmlStreamWriter>
+#include <QXmlStreamReader>
 #include <QThread>
 
 #include "MainWindow.h"
@@ -35,33 +34,9 @@
 #endif
 
 class OscXmlServer;
-class OscXmlClient;
 class OscMessage;
 class MainWindow;
 class Board;
-
-class XmlHandler : public QObject, public QXmlDefaultHandler
-{
-  Q_OBJECT
-  public:
-    XmlHandler( MainWindow *mainWindow, OscXmlClient *xmlClient );
-    bool endElement( const QString & namespaceURI, const QString & localName, const QString & qName );
-    bool startElement( const QString & namespaceURI, const QString & localName,
-                        const QString & qName, const QXmlAttributes & atts );
-    bool error (const QXmlParseException & exception);
-    bool fatalError (const QXmlParseException & exception);
-
-  signals:
-    void msg(const QStringList & msg, MsgType::Type, const QString & from);
-
-  private:
-    MainWindow *mainWindow;
-    OscXmlClient *xmlClient;
-    OscMessage* currentMessage;
-    QString currentDestination;
-    int currentPort;
-    QList<OscMessage*> oscMessageList;
-};
 
 class OscXmlClient : public QThread
 {
@@ -71,7 +46,6 @@ class OscXmlClient : public QThread
     ~OscXmlClient( ) { }
     void run();
     void sendCrossDomainPolicy();
-    void resetParser() { lastParseComplete = true; }
 
   public slots:
     void boardListUpdate( const QList<Board*> & boardList, bool arrived );
@@ -85,15 +59,15 @@ class OscXmlClient : public QThread
   private:
     int socketDescriptor;
     MainWindow *mainWindow;
-    bool lastParseComplete;
     QXmlStreamWriter xmlWriter;
-    QXmlSimpleReader xml;
-    QXmlInputSource xmlInput;
-    XmlHandler *handler;
+    QXmlStreamReader xmlReader;
     QTcpSocket *socket;
-    QList<QString> uiMessages;
-    QString peerAddress;
     bool shuttingDown;
+
+    OscMessage* currentMessage;
+    QString currentDestination;
+    int currentPort;
+    QList<OscMessage*> oscMessageList;
 
     bool isConnected( );
 
